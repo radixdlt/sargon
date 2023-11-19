498f7834dd27c41290ae1e9923ebea79c1b7a8f0
Alexander Cyon <alex.cyon@gmail.com>
2023-11-19 20:47:40 +0100

    Add JSON test for third party deposits

--- profile/src/v100/entity/account/on_ledger_settings/third_party_deposits/third_party_deposits.rs
+++ profile/src/v100/entity/account/on_ledger_settings/third_party_deposits/third_party_deposits.rs
@@ -68,4 +68,68 @@ impl ThirdPartyDeposits {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::collections::BTreeSet;
+
+    use wallet_kit_common::json::assert_eq_after_json_roundtrip;
+
+    use crate::v100::{
+        address::non_fungible_global_id::NonFungibleGlobalId,
+        entity::account::on_ledger_settings::third_party_deposits::{
+            asset_exception::AssetException,
+            deposit_address_exception_rule::DepositAddressExceptionRule, deposit_rule::DepositRule,
+            depositor_address::DepositorAddress,
+        },
+    };
+
+    use super::ThirdPartyDeposits;
+
+    #[test]
+    fn json_roundtrip() {
+        let excp1 = AssetException::new(
+            "resource_rdx1tkk83magp3gjyxrpskfsqwkg4g949rmcjee4tu2xmw93ltw2cz94sq"
+                .try_into()
+                .unwrap(),
+            DepositAddressExceptionRule::Deny,
+        );
+        let excp2 = AssetException::new(
+            "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd"
+                .try_into()
+                .unwrap(),
+            DepositAddressExceptionRule::Allow,
+        );
+        let model = ThirdPartyDeposits::with_rule_and_lists(
+            DepositRule::AcceptKnown,
+            BTreeSet::from_iter([excp1, excp2].into_iter()),
+            BTreeSet::from_iter(
+                [DepositorAddress::NonFungibleGlobalID(NonFungibleGlobalId::try_from_str("resource_sim1ngktvyeenvvqetnqwysevcx5fyvl6hqe36y3rkhdfdn6uzvt5366ha:<foobar>").unwrap())]
+                .into_iter(),
+            ),
+        );
+
+        assert_eq_after_json_roundtrip(
+            &model,
+            r#"
+        {
+	"depositRule" : "acceptKnown",
+	"assetsExceptionList" : [
+		{
+			"address" : "resource_rdx1tkk83magp3gjyxrpskfsqwkg4g949rmcjee4tu2xmw93ltw2cz94sq",
+			"exceptionRule" : "deny"
+		},
+		{
+			"address" : "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd",
+			"exceptionRule" : "allow"
+		}
+	],
+	"depositorsAllowList" : [
+		{
+			"value" : "resource_sim1ngktvyeenvvqetnqwysevcx5fyvl6hqe36y3rkhdfdn6uzvt5366ha:<foobar>",
+			"discriminator" : "nonFungibleGlobalID"
+		}
+	]
+}
+        "#,
+        );
+    }
+}
