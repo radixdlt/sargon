3ba563a4999b2bda0c4127c3f0f1484209074f71
Alexander Cyon <alex.cyon@gmail.com>
2023-12-03 21:21:19 +0100

    false negatives

--- hierarchical_deterministic/src/cap26/cap26_path/paths/is_entity_path.rs
+++ hierarchical_deterministic/src/cap26/cap26_path/paths/is_entity_path.rs
@@ -14,12 +14,17 @@ pub trait IsEntityPath: CAP26Repr {
 pub trait HasEntityPath<Path: IsEntityPath> {
     fn path(&self) -> Path;
 
+    #[cfg(not(tarpaulin_include))] // false negative
     fn network_id(&self) -> NetworkID {
         self.path().network_id()
     }
+
+    #[cfg(not(tarpaulin_include))] // false negative
     fn key_kind(&self) -> CAP26KeyKind {
         self.path().key_kind()
     }
+
+    #[cfg(not(tarpaulin_include))] // false negative
     fn index(&self) -> HDPathValue {
         self.path().index()
     }
