093f1aa80fd10382458341406d1ac9afcdf2dd7d
Alexander Cyon <alex.cyon@gmail.com>
2023-12-24 12:45:20 +0100

    Swift tests PublicKey

--- profile/src/wallet_kit_common/types/keys/slip10_curve.rs
+++ profile/src/wallet_kit_common/types/keys/slip10_curve.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 /// prefer using Curve25519 - which is incompatible with BIP32 (BIP44).
 ///
 /// For for information see [SLIP10 reference](https://github.com/satoshilabs/slips/blob/master/slip-0010.md)
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, uniffi::Enum)]
 #[serde(rename_all = "camelCase")]
 pub enum SLIP10Curve {
     /// Curve25519 which we use for Ed25519 for EdDSA signatures.
