3b63c69014d4c8f6658389ad74e2322bfa0c590a
Alexander Cyon <alex.cyon@gmail.com>
2024-01-26 09:14:08 +0100

    restucture bindgen test

--- profile/tests/uniffi/bindings/test_keys.swift
+++ profile/tests/uniffi/bindings/test_keys.swift
@@ -1,91 +1,5 @@
-// MARK: Data + Hex
 import Foundation
 import radix_wallet_kit
-
-protocol PublicKeyProtocol {
-	func toBytes() -> Data
-	func toHex() -> String
-}
-extension Ed25519PublicKey: PublicKeyProtocol {}
-extension Secp256k1PublicKey: PublicKeyProtocol {}
-
-extension Secp256k1PublicKey {
-	func toHex() -> String {
-		secp256k1PublicKeyToHex(publicKey: self)
-	}
-	func toBytes() -> Data {
-		secp256k1PublicKeyToBytes(publicKey: self)
-	}
-}
-extension Ed25519PublicKey {
-	func toHex() -> String {
-		ed25519PublicKeyToHex(publicKey: self)
-	}
-	func toBytes() -> Data {
-		ed25519PublicKeyToBytes(publicKey: self)
-	}
-
-}
-
-enum ByteHexEncodingErrors: Error {
-	case incorrectHexValue
-	case incorrectString
-}
-
-let charA = UInt8(UnicodeScalar("a").value)
-let char0 = UInt8(UnicodeScalar("0").value)
-private func htoi(_ value: UInt8) throws -> UInt8 {
-	switch value {
-	case char0...char0 + 9:
-		return value - char0
-	case charA...charA + 5:
-		return value - charA + 10
-	default:
-		throw ByteHexEncodingErrors.incorrectHexValue
-	}
-}
-
-private func itoh(_ value: UInt8) -> UInt8 {
-	return (value > 9) ? (charA + value - 10) : (char0 + value)
-}
-
-extension DataProtocol {
-	var hex: String {
-		let hexLen = self.count * 2
-		var hexChars = [UInt8](repeating: 0, count: hexLen)
-		var offset = 0
-
-		self.regions.forEach { (_) in
-			for i in self {
-				hexChars[Int(offset * 2)] = itoh((i >> 4) & 0xF)
-				hexChars[Int(offset * 2 + 1)] = itoh(i & 0xF)
-				offset += 1
-			}
-		}
-
-		return String(bytes: hexChars, encoding: .utf8)!
-	}
-}
-
-extension Data {
-	init(hex hexString: String) throws {
-		self.init()
-
-		if hexString.count % 2 != 0 || hexString.count == 0 {
-			throw ByteHexEncodingErrors.incorrectString
-		}
-
-		let stringBytes: [UInt8] = Array(
-			hexString.lowercased().data(using: String.Encoding.utf8)!)
-
-		for i in stride(from: stringBytes.startIndex, to: stringBytes.endIndex - 1, by: 2) {
-			let char1 = stringBytes[i]
-			let char2 = stringBytes[i + 1]
-
-			try self.append(htoi(char1) << 4 + htoi(char2))
-		}
-	}
-}
 
 func testKey<K: PublicKeyProtocol>(
 	type: K.Type = K.self,
@@ -144,3 +58,89 @@ func test() throws {
 }
 
 try! test()
+
+protocol PublicKeyProtocol {
+	func toBytes() -> Data
+	func toHex() -> String
+}
+extension Ed25519PublicKey: PublicKeyProtocol {}
+extension Secp256k1PublicKey: PublicKeyProtocol {}
+
+extension Secp256k1PublicKey {
+	func toHex() -> String {
+		secp256k1PublicKeyToHex(publicKey: self)
+	}
+	func toBytes() -> Data {
+		secp256k1PublicKeyToBytes(publicKey: self)
+	}
+}
+extension Ed25519PublicKey {
+	func toHex() -> String {
+		ed25519PublicKeyToHex(publicKey: self)
+	}
+	func toBytes() -> Data {
+		ed25519PublicKeyToBytes(publicKey: self)
+	}
+
+}
+
+enum ByteHexEncodingErrors: Error {
+	case incorrectHexValue
+	case incorrectString
+}
+
+let charA = UInt8(UnicodeScalar("a").value)
+let char0 = UInt8(UnicodeScalar("0").value)
+
+private func htoi(_ value: UInt8) throws -> UInt8 {
+	switch value {
+	case char0...char0 + 9:
+		return value - char0
+	case charA...charA + 5:
+		return value - charA + 10
+	default:
+		throw ByteHexEncodingErrors.incorrectHexValue
+	}
+}
+
+private func itoh(_ value: UInt8) -> UInt8 {
+	return (value > 9) ? (charA + value - 10) : (char0 + value)
+}
+
+extension DataProtocol {
+	var hex: String {
+		let hexLen = self.count * 2
+		var hexChars = [UInt8](repeating: 0, count: hexLen)
+		var offset = 0
+
+		self.regions.forEach { (_) in
+			for i in self {
+				hexChars[Int(offset * 2)] = itoh((i >> 4) & 0xF)
+				hexChars[Int(offset * 2 + 1)] = itoh(i & 0xF)
+				offset += 1
+			}
+		}
+
+		return String(bytes: hexChars, encoding: .utf8)!
+	}
+}
+
+extension Data {
+	init(hex hexString: String) throws {
+		self.init()
+
+		if hexString.count % 2 != 0 || hexString.count == 0 {
+			throw ByteHexEncodingErrors.incorrectString
+		}
+
+		let stringBytes: [UInt8] = Array(
+			hexString.lowercased().data(using: String.Encoding.utf8)!)
+
+		for i in stride(from: stringBytes.startIndex, to: stringBytes.endIndex - 1, by: 2) {
+			let char1 = stringBytes[i]
+			let char2 = stringBytes[i + 1]
+
+			try self.append(htoi(char1) << 4 + htoi(char2))
+		}
+	}
+}
