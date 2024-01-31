6606f9427993b89b994de4b6dcdfdc623b4e69be
Alexander Cyon <alex.cyon@rdx.works>
2024-01-31 15:28:15 +0100

    Passing JSON roundtrip tests with Profile with Authorized Dapps

--- profile/src/v100/networks/network/profile_network.rs
+++ profile/src/v100/networks/network/profile_network.rs
@@ -17,6 +17,7 @@ pub struct ProfileNetwork {
     /// An ordered set of Personas on this network.
     pub personas: Personas,
 
+    #[serde(rename = "authorizedDapps")]
     pub authorized_dapps: AuthorizedDapps,
 }
 
@@ -410,6 +411,62 @@ mod tests {
 							]
 						}
 					}
+				],
+				"authorizedDapps": [
+					{
+						"networkID": 1,
+						"dAppDefinitionAddress": "account_rdx12x0xfz2yumu2qsh6yt0v8xjfc7et04vpsz775kc3yd3xvle4w5d5k5",
+						"displayName": "Radix Dashboard",
+						"referencesToAuthorizedPersonas": [
+							{
+								"identityAddress": "identity_rdx122kttqch0eehzj6f9nkkxcw7msfeg9udurq5u0ysa0e92c59w0mg6x",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "exactly",
+										"quantity": 2
+									},
+									"ids": [
+										"account_rdx16xlfcpp0vf7e3gqnswv8j9k58n6rjccu58vvspmdva22kf3aplease",
+										"account_rdx16yf8jxxpdtcf4afpj5ddeuazp2evep7quuhgtq28vjznee08master"
+									]
+								}
+							},
+							{
+								"identityAddress": "identity_rdx12gcd4r799jpvztlffgw483pqcen98pjnay988n8rmscdswd872xy62",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "atLeast",
+										"quantity": 1
+									},
+									"ids": [
+										"account_rdx16yf8jxxpdtcf4afpj5ddeuazp2evep7quuhgtq28vjznee08master"
+									]
+								}
+							}
+						]
+					},
+					{
+						"networkID": 1,
+						"dAppDefinitionAddress": "account_rdx12xuhw6v30chdkhcu7qznz9vu926vxefr4h4tdvc0mdckg9rq4afx9t",
+						"displayName": "Gumball Club",
+						"referencesToAuthorizedPersonas": [
+							{
+								"identityAddress": "identity_rdx12gcd4r799jpvztlffgw483pqcen98pjnay988n8rmscdswd872xy62",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "atLeast",
+										"quantity": 1
+									},
+									"ids": [
+										"account_rdx16yf8jxxpdtcf4afpj5ddeuazp2evep7quuhgtq28vjznee08master"
+									]
+								}
+							}
+						]
+					}
 				]
 			}
             "#,
@@ -632,6 +689,62 @@ mod tests {
 								}
 							]
 						}
+					}
+				],
+				"authorizedDapps": [
+					{
+						"networkID": 2,
+						"dAppDefinitionAddress": "account_tdx_2_128evrrwfp8gj9240qq0m06ukhwaj2cmejluxxreanzjwq62vmlf8r4",
+						"displayName": "Dev Console",
+						"referencesToAuthorizedPersonas": [
+							{
+								"identityAddress": "identity_tdx_2_12fk6qyu2860xyx2jk7j6ex464ccrnxrve4kpaa8qyxx99y5627ahhc",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "exactly",
+										"quantity": 2
+									},
+									"ids": [
+										"account_tdx_2_1289zm062j788dwrjefqkfgfeea5tkkdnh8htqhdrzdvjkql4kxceql",
+										"account_tdx_2_129663ef7fj8azge3y6sl73lf9vyqt53ewzlf7ul2l76mg5wyqlqlpr"
+									]
+								}
+							},
+							{
+								"identityAddress": "identity_tdx_2_12gr0d9da3jvye7mdrreljyqs35esjyjsl9r8t5v96hq6fq367cln08",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "atLeast",
+										"quantity": 1
+									},
+									"ids": [
+										"account_tdx_2_129663ef7fj8azge3y6sl73lf9vyqt53ewzlf7ul2l76mg5wyqlqlpr"
+									]
+								}
+							}
+						]
+					},
+					{
+						"networkID": 2,
+						"dAppDefinitionAddress": "account_tdx_2_12yf9gd53yfep7a669fv2t3wm7nz9zeezwd04n02a433ker8vza6rhe",
+						"displayName": "Sandbox",
+						"referencesToAuthorizedPersonas": [
+							{
+								"identityAddress": "identity_tdx_2_12gr0d9da3jvye7mdrreljyqs35esjyjsl9r8t5v96hq6fq367cln08",
+								"lastLogin": "2024-01-31T14:23:45.000Z",
+								"sharedAccounts": {
+									"request": {
+										"quantifier": "atLeast",
+										"quantity": 1
+									},
+									"ids": [
+										"account_tdx_2_129663ef7fj8azge3y6sl73lf9vyqt53ewzlf7ul2l76mg5wyqlqlpr"
+									]
+								}
+							}
+						]
 					}
 				]
 			}
