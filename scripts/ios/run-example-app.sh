8589383286f396d534b11207df3e6b487646bfc7
Alexander Cyon <alex.cyon@gmail.com>
2024-02-15 11:59:39 +0100

    improve script starting app

--- scripts/ios/run-example-app.sh
+++ scripts/ios/run-example-app.sh
@@ -16,27 +16,24 @@ BUNDLE_IDENTIFIER=works.rdx.planbok.Planbok;
 APP_SCHEME_NAME=PlanbokApp
 APP_XCODEPROJP_PATH=examples/iOS/App/$APP_SCHEME_NAME.xcodeproj
 TARGET_IOS_VERSION=17.2
-TARGET_IPHONE_NAME="iPhone 15"
-IPHONE_SIM_DEST="platform=iOS Simulator,name=$TARGET_IPHONE_NAME,OS=$TARGET_IOS_VERSION"
+TARGET_IPHONE_NAME="iPhone 15 Pro Max"
 
-echo "📦 Start of '$me' (see: '$DIR/$me')"
-echo "IPHONE_SIM_DEST: $IPHONE_SIM_DEST"
+echo "✨📱 Start of '$me' (see: '$DIR/$me')"
 cd "$PARENT_DIRECTORY" # go to parent of parent, which is project root.
-
-BUILD_ROOT=$(xcodebuild -project $APP_XCODEPROJP_PATH -scheme $APP_SCHEME_NAME -destination $IPHONE_SIM_DEST build -quiet -showBuildSettings CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | grep -m 1 'BUILD_ROOT' | grep -oEi "\/.*")
-
-
-echo "build root is: '$BUILD_ROOT'"
-APP_PATH="$BUILD_ROOT/Debug-iphonesimulator/$APP_SCHEME_NAME.app"
 
 export SED_START="iOS $TARGET_IOS_VERSION"
 export SED_END="tvOS"
+
 export IOS_SIM_UDID=$(xcrun simctl list devices | sed -n "/$SED_START/,/$SED_END/p" | grep "$TARGET_IPHONE_NAME (" | grep -E -o -i "([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})");\
 
-xcrun simctl boot "$IOS_SIM_UDID";\
-xcrun simctl launch booted "$BUNDLE_IDENTIFIER"
+IPHONE_SIM_DEST="platform=iOS,id=$IOS_SIM_UDID"
 
-echo "Running sim for $APP_SCHEME_NAME - $BUNDLE_IDENTIFIER"
+BUILD_ROOT=$(xcodebuild -project $APP_XCODEPROJP_PATH -scheme $APP_SCHEME_NAME -destination $IPHONE_SIM_DEST build -quiet -showBuildSettings CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | grep -m 1 'BUILD_ROOT' | grep -oEi "\/.*")
+
+APP_PATH="$BUILD_ROOT/Debug-iphonesimulator/$APP_SCHEME_NAME.app"
+
+xcrun simctl boot "$IOS_SIM_UDID" > /dev/null 2>&1;
+xcrun simctl launch booted "$BUNDLE_IDENTIFIER" > /dev/null 2>&1;
 
 # Open the simulator
 open -a 'Simulator' --args -CurrentDeviceUDID $IOS_SIM_UDID
@@ -52,7 +49,6 @@ do
 	sleep 1
 done
 
-echo "Installing app at path $APP_PATH"
 xcrun simctl install booted $APP_PATH
 
 if [[ $DEBUGGER_ENABLED == "1" ]]; then
@@ -69,3 +65,5 @@ echo "Starting Sim for $APP_PATH" > $LOG_FILE
 # Launch the app program into the booted sim
 # - Run in the background
 `xcrun simctl launch $LAUNCH_DEBUGGER_ENABLED_FLAG $USE_CONSOLE_FLAG booted $BUNDLE_IDENTIFIER 2>&1 >> $LOG_FILE` &
+
+echo "✨📱 End of '$me' (see: '$DIR/$me')"
