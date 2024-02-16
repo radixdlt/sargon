274951a29ec630470a79a7e78d8a4135f622a2e5
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-02-16 19:40:37 +0200

    Implement building sargon from gradle

--- android/sargon/build.gradle.kts
+++ android/sargon/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.configurationcache.extensions.capitalized
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -34,7 +36,7 @@ android {
 }
 
 cargoNdk {
-    module  = "../sargon"  // Directory containing Cargo.toml
+    module = "../sargon"  // Directory containing Cargo.toml
     librariesNames = arrayListOf("libsargon.so")
 }
 
@@ -42,6 +44,98 @@ dependencies {
     implementation(libs.jna) {
         artifact {
             type = "aar"
+        }
+    }
+}
+
+sealed interface TargetTriple {
+    val name: String
+    val rustTarget: String
+    val nativeTarget: String
+
+    val linkerPath: String
+    val ccPath: String
+    val arPath: String
+
+    fun environment(ndkHome: String): Map<String, String> {
+        val linkerVar = rustTarget.uppercase().replace("-", "_")
+        return mapOf(
+            "CARGO_TARGET_${linkerVar}_LINKER" to "$ndkHome/$linkerPath",
+            "CC" to "$ndkHome/$ccPath",
+            "AR" to "$ndkHome/$arPath"
+        )
+    }
+
+    object Aarch64 : TargetTriple {
+        override val name: String = "Aarch64"
+        override val rustTarget: String = "aarch64-linux-android"
+        override val nativeTarget: String = "arm64-v8a"
+        override val linkerPath: String =
+            "toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang"
+        override val ccPath: String =
+            "toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang"
+        override val arPath: String = "toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ar"
+    }
+
+    object ArmeabiV7a : TargetTriple {
+        override val name: String = "ArmeabiV7a"
+        override val rustTarget: String = "armv7-linux-androideabi"
+        override val nativeTarget: String = "armeabi-v7a"
+        override val linkerPath: String =
+            "toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang"
+        override val ccPath: String =
+            "toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang"
+        override val arPath: String = "toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ar"
+    }
+
+    companion object {
+        val all: List<TargetTriple> = listOf(Aarch64, ArmeabiV7a)
+    }
+}
+
+android.libraryVariants.all {
+    val variantName = this.name
+    val isDebug = variantName == "debug"
+
+    // Register compile tasks
+    TargetTriple.all.forEach { triple ->
+        tasks.register("compile${variantName.capitalized()}${triple.name}") {
+            onlyIf {
+                (System.getenv("SDKROOT") != null).also {
+                    if (!it) System.err.println("No 'SDKROOT' env found")
+                }
+            }
+            onlyIf {
+                (System.getenv("ANDROID_NDK_HOME") != null).also {
+                    if (!it) System.err.println("No 'ANDROID_NDK_HOME' env found")
+                }
+            }
+
+            doLast {
+                exec {
+                    workingDir = rootDir.parentFile
+                    environment(triple.environment(System.getenv("ANDROID_NDK_HOME")))
+                    commandLine(
+                        "cargo",
+                        "build",
+                        if (isDebug) "" else "--release",
+                        "--target",
+                        triple.rustTarget
+                    )
+                }
+                exec {
+                    workingDir = rootDir
+                    commandLine("mkdir", "-p", "sargon/src/main/jniLibs/${triple.nativeTarget}")
+                }
+                exec {
+                    workingDir = rootDir.parentFile
+                    commandLine(
+                        "cp",
+                        "target/${triple.rustTarget}/$variantName/libsargon.so",
+                        "android/sargon/src/main/jniLibs/${triple.nativeTarget}/libsargon.so"
+                    )
+                }
+            }
         }
     }
 }
