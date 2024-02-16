aefd2d3a554286bd118d3e65f8610c02176b0982
Alexander Cyon <alex.cyon@rdx.works>
2024-02-16 13:52:40 +0100

    Readme aboult release

--- scripts/ios/release.sh
+++ scripts/ios/release.sh
@@ -10,7 +10,4 @@ DIR="$( cd "$( dirname "$REL_DIR" )" && pwd )";
 
 ZIP_PATH=$(sh $DIR/build-sargon.sh --release | tail -n 1)
 
-# This print MUST be the last print.
-# The path is read by `release.yml` CD pipeline!
-# This is probably terrible... but I'm a Rust/Swift dev, not a bash script dev...
-echo "$ZIP_PATH"
+gh release
