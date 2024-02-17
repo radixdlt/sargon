a9b96d013693837a7f195305203c963ca6c0b819
Alexander Cyon <alex.cyon@gmail.com>
2024-02-17 15:21:25 +0100

    Make Decimal192 conform to Comparable

--- apple/Sources/Sargon/Extensions/Methods/Decimal192+Wrap+Functions.swift
+++ apple/Sources/Sargon/Extensions/Methods/Decimal192+Wrap+Functions.swift
@@ -9,3 +9,36 @@ extension Decimal192: CustomStringConvertible {
 		decimalToString(decimal: self)
 	}
 }
+
+extension Decimal192: Comparable {
+	public static func > (lhs: Self, rhs: Self) -> Bool {
+		lhs.greaterThan(rhs)
+	}
+	public static func < (lhs: Self, rhs: Self) -> Bool {
+		lhs.lessThan(rhs)
+	}
+	public static func >= (lhs: Self, rhs: Self) -> Bool {
+		lhs.greaterThanOrEqual(rhs)
+	}
+	public static func <= (lhs: Self, rhs: Self) -> Bool {
+		lhs.lessThanOrEqual(rhs)
+	}
+}
+extension Decimal192 {
+
+	public func lessThan(other: Self) -> Bool {
+		decimalLessThan(lhs: self, rhs: other)
+	}
+
+	public func lessThanOrEqual(other: Self) -> Bool {
+		decimalLessThanOrEqual(lhs: self, rhs: other)
+	}
+
+	public func greaterThan(other: Self) -> Bool {
+		decimalGreaterThan(lhs: self, rhs: other)
+	}
+
+	public func greaterThanOrEqual(other: Self) -> Bool {
+		decimalGreaterThanOrEqual(lhs: self, rhs: other)
+	}
+}
