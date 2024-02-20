dd4eda66c4341c593dc6c55b92fbbc40bc248761
Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
2024-02-20 10:42:30 +0100

    Git add before git commit (#9)

--- scripts/ios/release.sh
+++ scripts/ios/release.sh
@@ -36,8 +36,15 @@ XCFRAME_ZIP_PATH=`echo "$OUTPUT_OF_BUILD" | cut -d ";" -f 2` || exit $?
 
 echo "🚢  CHECKSUM: $CHECKSUM"
 echo "🚢  XCFRAME_ZIP_PATH: $XCFRAME_ZIP_PATH"
-GIT_COMMIT_CMD="git commit -m \"Release of '$NEXT_TAG' (updated Package.swift with new checksum and path to zip on Github). This commit is not merged into main/develop branch (and need not be).\" --no-verify"
+
+GIT_ADD_CMD="git add Package.swift apple/Sources/UniFFI/Sargon.swift"
+echo "🚢  Staging (potentially) changed files with: $GIT_ADD_CMD"
+eval $GIT_ADD_CMD
+
+GIT_COMMIT_CMD="git commit -m \"Release of '$NEXT_TAG' (updated Package.swift with new checksum and path to zip on Github, and maybe apple/Sources/UniFFI/Sargon.swift). This commit is not merged into main/develop branch (and need not be).\" --no-verify"
+echo "🚢  Git commiting changes to Package.swift (and maybe Sargon.swift)"
 eval $GIT_COMMIT_CMD
+
 `git tag $NEXT_TAG`
 echo "🚢 🏷️ 📡 Pushing tag: $(NEXT_TAG), but only tag, not commit."
 `git push origin $NEXT_TAG`
