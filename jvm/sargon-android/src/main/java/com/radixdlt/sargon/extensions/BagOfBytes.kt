96746616a464ef07d853f24a178b68d37c113e98
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-05 17:59:09 +0200

    Add BagOfBytes test

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/BagOfBytes.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/BagOfBytes.kt
@@ -1,8 +1,22 @@
+@file:OptIn(ExperimentalUnsignedTypes::class, ExperimentalStdlibApi::class)
+
 package com.radixdlt.sargon.extensions
 
+import com.radixdlt.sargon.BagOfBytes
 import com.radixdlt.sargon.newBagOfBytesFrom
 import kotlin.random.Random
 
 fun bagOfBytesOf(byteArray: ByteArray) = newBagOfBytesFrom(bytes = byteArray)
 
-fun randomBagOfBytes(byteCount: Int) = Random.nextBytes(size = byteCount)
+fun randomBagOfBytes(byteCount: Int) = Random.nextBytes(size = byteCount).toBagOfBytes()
+
+fun String.hexToBagOfBytes(): BagOfBytes {
+    check(length % 2 == 0) { "Must have an even length" }
+
+    return chunked(2).map { it.toInt(16).toByte() }.toByteArray().toBagOfBytes()
+}
+
+fun ByteArray.toBagOfBytes() = newBagOfBytesFrom(bytes = this)
+
+val BagOfBytes.hex: String
+    get() = toUByteArray().toHexString()
