bb3331f0253b5acbf83c32a535645eea411c5e74
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-05 17:59:31 +0200

    Rename test method

--- jvm/sargon-android/src/test/java/com/radixdlt/sargon/AccountAddressTest.kt
+++ jvm/sargon-android/src/test/java/com/radixdlt/sargon/AccountAddressTest.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test
 class AccountAddressTest {
 
     @Test
-    fun testAccountAddress() {
+    fun test() {
         val bech32 = "account_rdx129qdd2yp9vs8jkkn2uwn6sw0ejwmcwr3r4c3usr2hp0nau67m2kzdm"
         val key = newEd25519PublicKeyFromHex(
             hex = "3e9b96a2a863f1be4658ea66aa0584d2a8847d4c0f658b20e62e3594d994d73d"
