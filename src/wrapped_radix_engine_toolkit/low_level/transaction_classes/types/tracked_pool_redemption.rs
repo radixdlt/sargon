73174f74a682e9705b42a998388638717e7cd62b
Alexander Cyon <alex.cyon@rdx.works>
2024-03-08 15:10:56 +0100

    Add Execution Summary test for Pool Redemption class

--- src/wrapped_radix_engine_toolkit/low_level/transaction_classes/types/tracked_pool_redemption.rs
+++ src/wrapped_radix_engine_toolkit/low_level/transaction_classes/types/tracked_pool_redemption.rs
@@ -14,18 +14,30 @@ pub struct TrackedPoolRedemption {
     pub redeemed_resources: HashMap<ResourceAddress, Decimal192>,
 }
 
+impl TrackedPoolRedemption {
+    pub fn new(
+        pool_address: impl Into<PoolAddress>,
+        pool_units_resource_address: impl Into<ResourceAddress>,
+        pool_units_amount: impl Into<Decimal192>,
+        redeemed_resources: impl Into<HashMap<ResourceAddress, Decimal192>>,
+    ) -> Self {
+        Self {
+            pool_address: pool_address.into(),
+            pool_units_resource_address: pool_units_resource_address.into(),
+            pool_units_amount: pool_units_amount.into(),
+            redeemed_resources: redeemed_resources.into(),
+        }
+    }
+}
+
 impl From<(RetTrackedPoolRedemption, NetworkID)> for TrackedPoolRedemption {
     fn from(value: (RetTrackedPoolRedemption, NetworkID)) -> Self {
         let (ret, n) = value;
-        Self {
-            pool_address: (ret.pool_address, n).into(),
-            pool_units_resource_address: (ret.pool_units_resource_address, n)
-                .into(),
-            pool_units_amount: ret.pool_units_amount.into(),
-            redeemed_resources: to_hashmap_network_aware_key(
-                ret.redeemed_resources,
-                n,
-            ),
-        }
+        Self::new(
+            (ret.pool_address, n),
+            (ret.pool_units_resource_address, n),
+            ret.pool_units_amount,
+            to_hashmap_network_aware_key(ret.redeemed_resources, n),
+        )
     }
 }
