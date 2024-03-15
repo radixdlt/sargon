921f655e15f8817f5e7ede395475628908d2770b
Anka <runner@Mac-1710502787160.local>
2024-03-15 13:41:16 +0000

    Release of '0.3.1' (updated Package.swift with new checksum and path to zip on Github, and maybe apple/Sources/UniFFI/Sargon.swift). This commit is not merged into main/develop branch (and need not be).

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ if useLocalFramework {
 		path: "./target/ios/libsargon-rs.xcframework"
 	)
 } else {
-	let releaseTag = "0.1.0"
-	let releaseChecksum = "befef7d56108305ff6ff69d67483471395c3e603e299b3b15f5a826328de272b"
+	let releaseTag = "0.3.1"
+	let releaseChecksum = "e0ddd8eee63aa4633bfd76a0f459d1b763ce865a11dbcd71c5efee9610cd3e32"
 	binaryTarget = .binaryTarget(
 		name: sargonBinaryTargetName,
 		url:
