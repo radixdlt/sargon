cd8243c499d40b2992939e610c2029502e9bdc96
Alexander Cyon <alex.cyon@rdx.works>
2024-03-21 15:30:41 +0100

    Swift: use show instead of export and .txt format.

--- scripts/ios/export_code_cov.sh
+++ scripts/ios/export_code_cov.sh
@@ -8,4 +8,4 @@ echo "🗂️  exporting report to $1"
 REL_DIR=$0:P
 DIR="$( cd "$( dirname "$REL_DIR" )" && pwd )";
 
-sh $DIR/base_display_code_cov.sh export -format="lcov" > $1
+sh $DIR/base_display_code_cov.sh show > $1
