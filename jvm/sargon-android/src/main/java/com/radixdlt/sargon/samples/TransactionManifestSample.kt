3bbf57ce896c717c112fb42af9ab023acb8b05e0
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-25 13:33:59 +0200

    Remove hardcoded manifests from tests

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/samples/TransactionManifestSample.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/samples/TransactionManifestSample.kt
@@ -2,7 +2,10 @@ package com.radixdlt.sargon.samples
 
 import androidx.annotation.VisibleForTesting
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.radixdlt.sargon.PerAssetTransfers
 import com.radixdlt.sargon.TransactionManifest
+import com.radixdlt.sargon.newPerAssetTransfersSample
+import com.radixdlt.sargon.newPerAssetTransfersSampleOther
 import com.radixdlt.sargon.newTransactionManifestSample
 import com.radixdlt.sargon.newTransactionManifestSampleOther
 
@@ -19,5 +22,21 @@ val TransactionManifest.Companion.sample: Sample<TransactionManifest>
 class TransactionManifestPreviewParameterProvider: PreviewParameterProvider<TransactionManifest> {
     override val values: Sequence<TransactionManifest>
         get() = TransactionManifest.sample.all.asSequence()
+
+}
+
+@VisibleForTesting
+val PerAssetTransfers.Companion.sample: Sample<PerAssetTransfers>
+    get() = object : Sample<PerAssetTransfers> {
+
+        override fun invoke(): PerAssetTransfers = newPerAssetTransfersSample()
+
+        override fun other(): PerAssetTransfers = newPerAssetTransfersSampleOther()
+
+    }
+
+class PerAssetTransfersPreviewParameterProvider: PreviewParameterProvider<PerAssetTransfers> {
+    override val values: Sequence<PerAssetTransfers>
+        get() = PerAssetTransfers.sample.all.asSequence()
 
 }
