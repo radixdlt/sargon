ab9ca6cc957c50249d304f7d9de28b3f61d33b3d
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-03-29 17:39:16 +0200

    Add dependency information sample

---
+++ jvm/sargon-android/src/test/java/com/radixdlt/sargon/DependencyInformationTest.kt
@@ -0,0 +1,17 @@
+package com.radixdlt.sargon
+
+import com.radixdlt.sargon.extensions.string
+import com.radixdlt.sargon.samples.Sample
+import com.radixdlt.sargon.samples.sample
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class DependencyInformationTest: SampleTestable<DependencyInformation> {
+    override val samples: List<Sample<DependencyInformation>>
+        get() = listOf(DependencyInformation.sample)
+
+    @Test
+    fun testString() {
+        assertEquals("develop", DependencyInformation.sample().string)
+    }
+}
