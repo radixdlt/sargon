b78ff0a44f7df689b946d82b21e80a72597538bc
micbakos-rdx <125959264+micbakos-rdx@users.noreply.github.com>
2024-04-03 13:03:03 +0300

    Implement Intent/SignedIntent Hash formatted method (#56)

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/IntentHash.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/IntentHash.kt
@@ -1,8 +1,13 @@
 package com.radixdlt.sargon.extensions
 
+import com.radixdlt.sargon.AddressFormat
 import com.radixdlt.sargon.IntentHash
+import com.radixdlt.sargon.intentHashFormatted
 import com.radixdlt.sargon.newIntentHashFromString
 
 @Throws(SargonException::class)
 fun IntentHash.Companion.init(string: String) =
     newIntentHashFromString(string = string)
+
+fun IntentHash.formatted(format: AddressFormat = AddressFormat.DEFAULT) =
+    intentHashFormatted(address = this, format = format)
