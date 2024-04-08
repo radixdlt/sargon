c9983ecc951c03cd97e682004ed22246bedb9787
Alexander Cyon <alex.cyon@gmail.com>
2024-04-08 22:43:16 +0200

    fix wrong error kind being thrown from swift network antenna

--- apple/Tests/IntegrationTests/GatewayClientTests.swift
+++ apple/Tests/IntegrationTests/GatewayClientTests.swift
@@ -1,6 +1,6 @@
 final class GatewayClientTests: TestCase {
 	
-	func test_network() async throws {
+	func test_xrd_balance_of_account() async throws {
 		let gateway = GatewayClient(networkID: .mainnet)
 		let xrdBalance = try await gateway.xrdBalanceOfAccountOrZero(address: .sample)
 		XCTAssertGreaterThan(xrdBalance, 1)
