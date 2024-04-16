af9c8dd831dfdc6d6ea43f7981146c131623eef3
Alexander Cyon <alex.cyon@gmail.com>
2024-04-16 12:54:26 +0200

    JSON test of EncryptionScheme

--- src/profile/encrypted/encryption_scheme.rs
+++ src/profile/encrypted/encryption_scheme.rs
@@ -47,8 +47,8 @@ impl VersionedAlgorithm for EncryptionScheme {
 
 #[repr(u32)]
 #[derive(
-    Serialize,
-    Deserialize,
+    Serialize_repr,
+    Deserialize_repr,
     Clone,
     PartialEq,
     Eq,
@@ -61,8 +61,6 @@ pub enum EncryptionSchemeVersion {
 }
 
 #[derive(
-    Serialize,
-    Deserialize,
     Clone,
     Default,
     PartialEq,
@@ -79,15 +77,43 @@ impl EncryptionSchemeVersion1 {
         Self::DESCRIPTION.to_owned()
     }
 }
-impl VersionedEncryption for EncryptionSchemeVersion1 {
-    fn version(&self) -> EncryptionSchemeVersion {
-        EncryptionSchemeVersion::Version1
-    }
 
-    fn encrypt(
+impl Serialize for EncryptionSchemeVersion1 {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state =
+            serializer.serialize_struct("EncryptionSchemeVersion1", 2)?;
+        state.serialize_field("description", &self.description())?;
+        state.serialize_field("version", &self.version())?;
+        state.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for EncryptionSchemeVersion1 {
+    fn deserialize<D: Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<Self, D::Error> {
+        // https://github.com/serde-rs/serde/issues/1343#issuecomment-409698470
+        #[derive(Deserialize, Serialize)]
+        struct Wrapper {
+            version: EncryptionSchemeVersion,
+        }
+        let version = Wrapper::deserialize(deserializer).map(|w| w.version)?;
+        match version {
+            EncryptionSchemeVersion::Version1 => {
+                Ok(EncryptionSchemeVersion1::default())
+            }
+        }
+    }
+}
+
+impl EncryptionSchemeVersion1 {
+    fn seal(
         data: Vec<u8>,
         encryption_key: &Key<Aes256Gcm>,
-    ) -> Result<Vec<u8>> {
+    ) -> Result<AESSealedBox> {
         let cipher = Aes256Gcm::new(encryption_key);
         let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
         let cipher_text =
@@ -97,16 +123,13 @@ impl VersionedEncryption for EncryptionSchemeVersion1 {
             })?;
         let nonce = Exactly12Bytes::try_from(nonce.as_slice()).unwrap();
 
-        let sealed_box = AESSealedBox { nonce, cipher_text };
-
-        Ok(sealed_box.combined())
+        Ok(AESSealedBox { nonce, cipher_text })
     }
 
-    fn decrypt(
-        data: Vec<u8>,
+    fn open(
+        sealed_box: AESSealedBox,
         decryption_key: &Key<Aes256Gcm>,
     ) -> Result<Vec<u8>> {
-        let sealed_box = AESSealedBox::try_from(data)?;
         let cipher = Aes256Gcm::new(decryption_key);
         let cipher_text = sealed_box.cipher_text;
         cipher
@@ -118,6 +141,34 @@ impl VersionedEncryption for EncryptionSchemeVersion1 {
                 error!("Failed to AES decrypt data - error: {:?}", e);
                 CommonError::AESDecryptionFailed
             })
+    }
+}
+
+impl VersionedEncryption for EncryptionSchemeVersion1 {
+    fn version(&self) -> EncryptionSchemeVersion {
+        EncryptionSchemeVersion::Version1
+    }
+
+    fn encrypt(
+        data: Vec<u8>,
+        encryption_key: &Exactly32Bytes,
+    ) -> Result<Vec<u8>> {
+        Self::seal(data, &Key::<Aes256Gcm>::from(encryption_key.clone()))
+            .map(|sb| sb.combined())
+    }
+
+    fn decrypt(
+        data: Vec<u8>,
+        decryption_key: &Exactly32Bytes,
+    ) -> Result<Vec<u8>> {
+        let sealed_box = AESSealedBox::try_from(data)?;
+        Self::open(sealed_box, &Key::<Aes256Gcm>::from(decryption_key.clone()))
+    }
+}
+
+impl From<Exactly32Bytes> for Key<Aes256Gcm> {
+    fn from(value: Exactly32Bytes) -> Self {
+        Self::try_from(*value.bytes()).unwrap()
     }
 }
 
@@ -174,13 +225,45 @@ mod tests {
     type SUT = EncryptionSchemeVersion1;
 
     #[test]
+    fn json_() {
+        let model = SUT::default();
+        assert_eq_after_json_roundtrip(
+            &model,
+            r#"
+        {
+			"version": 1,
+			"description": "AESGCM-256"
+		}
+        "#,
+        );
+    }
+
+    #[test]
     fn encryption_roundtrip() {
-        let key = Aes256Gcm::generate_key(OsRng);
+        let key = Exactly32Bytes::generate();
         let msg = "Hello Radix";
         let msg_bytes = msg.bytes().collect();
         let encrypted = SUT::encrypt(msg_bytes, &key).unwrap();
         let decrypted_bytes = SUT::decrypt(encrypted, &key).unwrap();
         let decrypted = String::from_utf8(decrypted_bytes).unwrap();
         assert_eq!(msg, decrypted);
+    }
+
+    #[test]
+    fn decrypt_known() {
+        let test = |encrypted_hex: &str,
+                    key_hex: &str,
+                    expected_plaintext: &str| {
+            let decryption_key = Exactly32Bytes::from_str(key_hex).unwrap();
+            let encrypted = hex_decode(encrypted_hex).unwrap();
+            let decrypted = SUT::decrypt(encrypted, &decryption_key).unwrap();
+            assert_eq!(hex::encode(decrypted), expected_plaintext);
+        };
+
+        test(
+            "4c2266de48fd17a4bb52d5883751d054258755ce004154ea204a73a4c35e",
+            "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+            "abba",
+        );
     }
 }
--- src/profile/encrypted/versioned_encryption.rs
+++ src/profile/encrypted/versioned_encryption.rs
@@ -1,20 +1,14 @@
 use crate::prelude::*;
-
-use aes_gcm::{
-    aead::{Aead, AeadCore, KeyInit},
-    Aes256Gcm,
-    Key, // Or `Aes128Gcm`
-    Nonce,
-};
-
 pub trait VersionedEncryption {
     fn version(&self) -> EncryptionSchemeVersion;
+
     fn encrypt(
         data: Vec<u8>,
-        encryption_key: &Key<Aes256Gcm>,
+        encryption_key: &Exactly32Bytes,
     ) -> Result<Vec<u8>>;
+
     fn decrypt(
         data: Vec<u8>,
-        decryption_key: &Key<Aes256Gcm>,
+        decryption_key: &Exactly32Bytes,
     ) -> Result<Vec<u8>>;
 }
