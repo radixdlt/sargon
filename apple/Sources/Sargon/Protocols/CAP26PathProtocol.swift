e4a6f5013ad0bdbd8a141b49d8e547cfcbad201e
Alexander Cyon <alex.cyon@rdx.works>
2024-04-19 16:48:22 +0200

    derivation path tests

--- apple/Sources/Sargon/Protocols/CAP26PathProtocol.swift
+++ apple/Sources/Sargon/Protocols/CAP26PathProtocol.swift
@@ -1,9 +1,37 @@
-public protocol CAP26PathProtocol: HDPathProtocol {
+import SargonUniFFI
+
+public protocol DerivationPathProtocol: HDPathProtocol {
+    var asDerivationPath: DerivationPath { get }
+}
+
+extension DerivationPath: DerivationPathProtocol {
+    public init(string: String) throws {
+        if let cap26 = try? CAP26Path(string: string) {
+            self = .cap26(value: cap26)
+        } else {
+            self = try .bip44Like(value: .init(string: string))
+        }
+    }
+    public var asDerivationPath: DerivationPath { self }
+}
+
+public protocol CAP26PathProtocol: DerivationPathProtocol {
     var asGeneral: CAP26Path { get }
 }
 
 extension CAP26PathProtocol {
-    public var description: String {
+    public var asDerivationPath: DerivationPath {
+        .cap26(value: asGeneral)
+    }
+    public func toString() -> String {
         asGeneral.toString()
     }
 }
+
+extension CAP26PathProtocol {
+    public var description: String {
+      toString()
+    }
+}
+
+
---
+++ apple/Tests/TestCases/Crypto/DerivationPathTests.swift
@@ -0,0 +1,78 @@
+import CustomDump
+import Foundation
+import Sargon
+import SargonUniFFI
+import XCTest
+
+final class DerivationPathTests: HDPathProtocolTests<DerivationPath> {
+    func test_sample_description() {
+        XCTAssertNoDifference(SUT.sample.description, "m/44H/1022H/1H/525H/1460H/0H")
+    }
+    
+    func test_cap26_account_path_as_derivation_path() {
+        let sut = AccountPath.sample
+        
+        XCTAssertEqual(
+            sut.asDerivationPath,
+            DerivationPath.cap26(value: .account(value: sut))
+        )
+    }
+    
+    
+    func test_cap26_account_hd_path() {
+        let accountPath = AccountPath.sample
+        
+        XCTAssertEqual(
+            accountPath.asDerivationPath.path,
+            accountPath.path
+        )
+    }
+    
+    func test_cap26_hd_path() {
+        let accountPath = AccountPath.sample
+        
+        XCTAssertEqual(
+            accountPath.asDerivationPath.path,
+            accountPath.asGeneral.path
+        )
+    }
+    
+    func test_bip44_account_hd_path() {
+        let bip44Path = BIP44LikePath.sample
+        
+        XCTAssertEqual(
+            bip44Path.asDerivationPath.path,
+            bip44Path.path
+        )
+    }
+    
+    
+    func test_bip44_account_path_as_derivation_path() {
+        let bip44Path = BIP44LikePath.sample
+        
+        XCTAssertEqual(
+            bip44Path.asDerivationPath,
+            DerivationPath.bip44Like(value: bip44Path)
+        )
+    }
+    
+    func test_bip44_account_path_to_string() {
+        let bip44Path = BIP44LikePath.sample
+        
+        XCTAssertEqual(
+            bip44Path.asDerivationPath.toString(),
+            bip44Path.toString()
+        )
+    }
+    
+    func test_bip44_string() throws {
+        XCTAssertNoDifference(
+            try SUT(string: "m/44H/1022H/0H/0/0H"),
+            BIP44LikePath.sample.asDerivationPath
+        )
+    }
+    
+    func test_as_general_is_identity() {
+        XCTAssertEqual(SUT.sample.asDerivationPath, SUT.sample)
+    }
+}
