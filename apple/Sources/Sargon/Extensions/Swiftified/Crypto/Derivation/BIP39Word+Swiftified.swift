c343e791091952111bfaccd0b68df98351bd8458
Alexander Cyon <alex.cyon@gmail.com>
2024-04-24 13:26:41 +0200

    UF export bip39 words + more swift tests

--- apple/Sources/Sargon/Extensions/Swiftified/Crypto/Derivation/BIP39Word+Swiftified.swift
+++ apple/Sources/Sargon/Extensions/Swiftified/Crypto/Derivation/BIP39Word+Swiftified.swift
@@ -1,10 +1,12 @@
 import SargonUniFFI
 
 public typealias BIP39Word = Bip39Word
-
+extension BIP39Word: SargonModel {}
 extension BIP39Word: Identifiable {
+	
 	public typealias ID = U11
+	
 	public var id: ID {
-		self.index
+		index
 	}
 }
