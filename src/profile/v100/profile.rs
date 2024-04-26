94f8f5aa672e1726a2c5582b0f462dc0c2d5572a
Alexander Cyon <alex.cyon@rdx.works>
2024-04-26 12:10:26 +0200

    add analyze profile file contents

--- src/profile/v100/profile.rs
+++ src/profile/v100/profile.rs
@@ -46,6 +46,49 @@ pub struct Profile {
     pub networks: ProfileNetworks,
 }
 
+/// Result of analyzing a file (bytes), containing either a Profile
+/// which we were able to successfully JSON deserialize from the bytes,
+/// or EncryptedProfile for which wallets will continue prompting the
+/// user for an encryption password and then call JSON deserialize
+/// of `EncryptedProfileSnapshot` using [`Profile::new_from_encryption_bytes`](Profile::new_from_encryption_bytes)
+/// or if we failed to parse as Profile and `EncryptedProfileSnapshot`
+/// then `NotProfile` is used, indicating that the bytes is not at all
+/// a Profile.
+#[derive(Clone, PartialEq, Eq, Debug, Hash, uniffi::Enum)]
+#[allow(clippy::large_enum_variant)]
+pub enum ProfileFileContents {
+    /// The JSON deserialized Profile from some bytes.
+    PlaintextProfile(Profile),
+
+    /// We successfully JSON deserialized the bytes into
+    /// `EncryptedProfileSnapshot`, the wallets should proceed
+    /// with asking the user for the decryption password.
+    EncryptedProfile,
+
+    /// The bytes is neither a valid `Profile` nor `EncryptedProfile`,
+    /// it is either a corrupt file or it is not at all a Profile file,
+    /// contrary to the users beliefs (or the user accidentally selected
+    /// a random file...)
+    NotProfile,
+}
+
+impl Profile {
+    pub fn analyze_contents_of_file(
+        bytes: impl AsRef<[u8]>,
+    ) -> ProfileFileContents {
+        let json = bytes.as_ref();
+        if let Ok(profile) = Profile::new_from_json_bytes(json) {
+            return ProfileFileContents::PlaintextProfile(profile);
+        };
+
+        if serde_json::from_slice::<EncryptedProfileSnapshot>(json).is_ok() {
+            return ProfileFileContents::EncryptedProfile;
+        };
+
+        ProfileFileContents::NotProfile
+    }
+}
+
 impl Profile {
     pub fn description(&self) -> String {
         format!(
@@ -355,6 +398,28 @@ mod tests {
                 .items(),
             [SLIP10Curve::Curve25519]
         );
+    }
+
+    #[test]
+    fn test_analyze_contents_of_file_plaintext_profile() {
+        let sut = SUT::sample();
+        let bytes = sut.to_json_bytes();
+        let contents = SUT::analyze_contents_of_file(bytes);
+        assert_eq!(contents, ProfileFileContents::PlaintextProfile(sut));
+    }
+
+    #[test]
+    fn test_analyze_contents_of_file_encrypted_profile() {
+        let sut = SUT::sample();
+        let bytes = sut.to_encryption_bytes("super secret");
+        let contents = SUT::analyze_contents_of_file(bytes);
+        assert_eq!(contents, ProfileFileContents::EncryptedProfile);
+    }
+
+    #[test]
+    fn test_analyze_contents_of_file_not_profile() {
+        let contents = SUT::analyze_contents_of_file(Exactly32Bytes::sample());
+        assert_eq!(contents, ProfileFileContents::NotProfile);
     }
 
     #[test]
