e823560f9c8545efb22bc8819830aff3f8f79820
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2024-04-26 08:37:48 +0300

    wip

--- src/radix_connect/mobile/client.rs
+++ src/radix_connect/mobile/client.rs
@@ -1,22 +1,22 @@
 use super::relay_service::Service as RelayService;
 use crate::prelude::*;
 
-/// The Radix Connect Mobile client.
-/// This is the object that will be used by the mobile app to handle interactions sent over Radix Connect Relay.
-#[derive(uniffi::Object)]
-pub struct RadixConnectMobile {
-    relay_service: RelayService,
-}
+// The Radix Connect Mobile client.
+// This is the object that will be used by the mobile app to handle interactions sent over Radix Connect Relay.
+// #[derive(uniffi::Object)]
+// pub struct RadixConnectMobile {
+//     relay_service: RelayService,
+// }
 
-impl RadixConnectMobile {
-    #[uniffi::constructor]
-    pub fn new(network_antenna: Arc<dyn NetworkAntenna>) -> Self {
-        Self {
-            relay_service: RelayService::new(HttpClient { network_antenna }),
-        }
-    }
-}
+// impl RadixConnectMobile {
+//     #[uniffi::constructor]
+//     pub fn new(network_antenna: Arc<dyn NetworkAntenna>) -> Self {
+//         Self {
+//             relay_service: RelayService::new(HttpClient { network_antenna }),
+//         }
+//     }
+// }
 
-impl RadixConnectMobile {
-    // TBA public API that will be used by the Wallet to handle deep link interactions.
-}
+// impl RadixConnectMobile {
+//     // TBA public API that will be used by the Wallet to handle deep link interactions.
+// }
