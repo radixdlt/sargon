9d7f8b1ee7d9c598aeb77e53bab648bbbcc38e25
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2024-05-02 15:07:57 +0300

    Fix rebase errors

--- src/profile/v100/profile_uniffi_fn.rs
+++ src/profile/v100/profile_uniffi_fn.rs
@@ -105,7 +105,7 @@ mod uniffi_tests {
     #[test]
     fn to_string_and_debug_string() {
         assert_eq!(profile_to_string(&SUT::sample()).len(), 4314);
-        assert_eq!(profile_to_debug_string(&SUT::sample()).len(), 27754);
+        assert_eq!(profile_to_debug_string(&SUT::sample()).len(), 27076);
         assert_ne!(
             profile_to_debug_string(&SUT::sample()),
             profile_to_debug_string(&SUT::sample_other())
