8b14c6976ddae1cf04a5da8f92f99c419758d0ef
Anka <runner@Mac-1715271551316.local>
2024-05-09 18:42:59 +0000

    Release of '0.7.10' (updated Package.swift with new checksum and path to zip on Github, and maybe apple/Sources/UniFFI/Sargon.swift). This commit is not merged into main branch (and need not be).

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ var strictSwiftSettings: [SwiftSetting] = swiftSettings
 
 let sargonBinaryTargetName = "SargonCoreRS"
 let binaryTarget: Target
-let useLocalFramework = true
+let useLocalFramework = false
 
 if useLocalFramework {
 	binaryTarget = .binaryTarget(
@@ -27,8 +27,8 @@ if useLocalFramework {
 		.unsafeFlags(["-warnings-as-errors"])
 	)
 } else {
-	let releaseTag = "0.1.0"
-	let releaseChecksum = "befef7d56108305ff6ff69d67483471395c3e603e299b3b15f5a826328de272b"
+	let releaseTag = "0.7.10"
+	let releaseChecksum = "f18a8036147186357a16fce27475a49b9fabc3ff28d2307be07a9cc31bc05369"
 	binaryTarget = .binaryTarget(
 		name: sargonBinaryTargetName,
 		url:
