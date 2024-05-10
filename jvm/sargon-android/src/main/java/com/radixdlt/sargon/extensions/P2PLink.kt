a70545945ad2f9fc27114293f6add3048b5b58f7
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-05-10 17:13:42 +0300

    Fix kotlin extensions for P2PLink

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/P2PLink.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/P2PLink.kt
@@ -1,8 +1,8 @@
 package com.radixdlt.sargon.extensions
 
-import com.radixdlt.sargon.Hash
+import com.radixdlt.sargon.PublicKeyHash
 import com.radixdlt.sargon.P2pLink
 import com.radixdlt.sargon.p2pLinkId
 
-val P2pLink.id: Hash
+val P2pLink.id: PublicKeyHash
     get() = p2pLinkId(link = this)
