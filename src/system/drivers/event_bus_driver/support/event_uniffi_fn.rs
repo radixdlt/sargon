062eac3cd1b43eed15d255ab33b1006f262a8f60
Alexander Cyon <alex.cyon@gmail.com>
2024-05-13 20:17:26 +0200

    more tests

--- src/system/drivers/event_bus_driver/support/event_uniffi_fn.rs
+++ src/system/drivers/event_bus_driver/support/event_uniffi_fn.rs
@@ -4,3 +4,16 @@ use crate::prelude::*;
 pub fn event_kind(event: &Event) -> EventKind {
     event.kind()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(clippy::upper_case_acronyms)]
+    type SUT = Event;
+
+    #[test]
+    fn test_kind() {
+        assert_eq!(event_kind(&SUT::Booted), EventKind::Booted);
+    }
+}
