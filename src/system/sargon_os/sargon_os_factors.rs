eef6c727d1c4f96e17a712209c30ce8e9585645b
Alexander Cyon <alex.cyon@gmail.com>
2024-05-13 20:35:44 +0200

    more tests (factors)

--- src/system/sargon_os/sargon_os_factors.rs
+++ src/system/sargon_os/sargon_os_factors.rs
@@ -93,3 +93,70 @@ impl SargonOS {
             .await
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use actix_rt::time::timeout;
+    use std::{future::Future, time::Duration};
+
+    #[allow(clippy::upper_case_acronyms)]
+    type SUT = SargonOS;
+
+    #[actix_rt::test]
+    async fn test_load_private_device_factor_source_by_id() {
+        // ARRANGE
+        let mwp = MnemonicWithPassphrase::sample();
+        let factor_source_id = FactorSourceIDFromHash::new_for_device(&mwp);
+        let os = SUT::fast_boot_bdfs(mwp.clone()).await;
+
+        // ACT
+        let private = os
+            .with_timeout(|x| {
+                x.load_private_device_factor_source_by_id(&factor_source_id)
+            })
+            .await
+            .unwrap();
+
+        // ASSERT
+        assert_eq!(private.mnemonic_with_passphrase, mwp);
+    }
+
+    #[actix_rt::test]
+    async fn test_bdfs() {
+        // ARRANGE
+        let mwp = MnemonicWithPassphrase::sample();
+        let os = SUT::fast_boot_bdfs(mwp.clone()).await;
+
+        // ACT
+        let loaded = os.bdfs();
+
+        // ASSERT
+        assert_eq!(
+            loaded.factor_source_id(),
+            FactorSourceIDFromHash::new_for_device(&mwp).into()
+        );
+    }
+
+    #[actix_rt::test]
+    async fn test_mnemonic_with_passphrase_of_device_factor_source_by_factor_source_id(
+    ) {
+        // ARRANGE
+        let mwp = MnemonicWithPassphrase::sample();
+        let factor_source_id = FactorSourceIDFromHash::new_for_device(&mwp);
+        let id = FactorSourceID::from(factor_source_id);
+        let os = SUT::fast_boot_bdfs(mwp.clone()).await;
+
+        // ACT
+        let loaded = os
+      .with_timeout(|x| {
+          x.mnemonic_with_passphrase_of_device_factor_source_by_factor_source_id(&id)
+      })
+      .await
+      .unwrap();
+
+        // ASSERT
+        assert_eq!(loaded, mwp);
+    }
+}
