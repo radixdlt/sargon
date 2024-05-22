e159f0d864413b8ef194c3c7bbd05a0245ff0cfb
Alexander Cyon <alex.cyon@gmail.com>
2024-05-22 11:47:48 +0200

    wip bindgen

--- scripts/ios/build-sargon.sh
+++ scripts/ios/build-sargon.sh
@@ -43,8 +43,8 @@ generate_ffi() {
     local TARGET_FOR_DYLIB_PATH="aarch64-apple-darwin"
   else
     local TARGET_FOR_DYLIB_PATH="aarch64-apple-ios"
-  fi 
-  cargo run --features build-binary --bin sargon-bindgen generate --library target/$TARGET_FOR_DYLIB_PATH/release/lib$1.dylib --language swift --out-dir target/uniffi-xcframework-staging
+  fi
+  cargo run -p bindgen generate --library target/$TARGET_FOR_DYLIB_PATH/release/lib$1.dylib --language swift --out-dir target/uniffi-xcframework-staging
   mkdir -p apple/Sources/UniFFI/
   mv target/uniffi-xcframework-staging/*.swift apple/Sources/UniFFI/
   mv target/uniffi-xcframework-staging/$1FFI.modulemap target/uniffi-xcframework-staging/module.modulemap  # Convention requires this have a specific name
@@ -96,7 +96,7 @@ else
   echo "📦 Start of '$me' (see: '$DIR/$me')"
 fi
 
-cd "$DIR" 
+cd "$DIR"
 cd "../../" # go to parent of parent, which is project root.
 
 
