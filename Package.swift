60006952864e2b4fb1e511a83844694819132d04
Anka <runner@Mac-1716514988262.local>
2024-05-24 07:23:28 +0000

    Release of '1.0.1' (updated Package.swift with new checksum and path to zip on Github, and maybe apple/Sources/UniFFI/Sargon.swift). This commit is not merged into main branch (and need not be).

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ var swiftSettings: [SwiftSetting] = [
 
 let sargonBinaryTargetName = "SargonCoreRS"
 let binaryTarget: Target
-let useLocalFramework = true
+let useLocalFramework = false
 
 if useLocalFramework {
 	binaryTarget = .binaryTarget(
@@ -20,8 +20,8 @@ if useLocalFramework {
 		path: "./target/swift/libsargon-rs.xcframework"
 	)
 } else {
-	let releaseTag = "0.1.0"
-	let releaseChecksum = "befef7d56108305ff6ff69d67483471395c3e603e299b3b15f5a826328de272b"
+	let releaseTag = "1.0.1"
+	let releaseChecksum = "5d5d844be2a3d48ec4fb1c380b163903d5d99097333173f65d1af1be317a22b5"
 	binaryTarget = .binaryTarget(
 		name: sargonBinaryTargetName,
 		url:
