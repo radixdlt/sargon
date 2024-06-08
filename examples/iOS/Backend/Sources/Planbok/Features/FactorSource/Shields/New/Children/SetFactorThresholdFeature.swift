c1049e6ad9df085c8f8736b432fbf438a71edabc
Alexander Cyon <alex.cyon@gmail.com>
2024-06-08 10:16:33 +0200

    [no ci] WIP

--- examples/iOS/Backend/Sources/Planbok/Features/FactorSource/Shields/New/Children/SetFactorThresholdFeature.swift
+++ examples/iOS/Backend/Sources/Planbok/Features/FactorSource/Shields/New/Children/SetFactorThresholdFeature.swift
@@ -41,8 +41,22 @@ public struct SetFactorThresholdFeature {
 	}
 	
 	public enum Action: ViewAction {
-		public enum ViewAction {}
+		public enum ViewAction {
+			case changedThreshold(Int)
+		}
 		case view(ViewAction)
+	}
+	
+	public var body: some ReducerOf<Self> {
+		Reduce { state, action in
+			switch action {
+			case let .view(.changedThreshold(index)):
+				var index = max(index, 0)
+				index = min(index, state.options.count - 1)
+				state.threshold = state.options[index]
+				return .none
+			}
+		}
 	}
 }
 
@@ -56,24 +70,48 @@ extension SetFactorThresholdFeature {
 		public init(store: StoreOf<HostingFeature>) {
 			self.store = store
 		}
-
+		public static let cellWidth: CGFloat = 70
 		public var body: some SwiftUI.View {
 			VStack(alignment: .center) {
 				Text("Choose the number of security factors required for \(store.role.title)")
-				
-				ScrollView {
-					HStack {
-						ForEach(store.options, id: \.self) { option in
-							VStack {
-								Text("\(option)").font(.largeTitle)
-									.fontWeight(.bold)
-									.foregroundStyle(option == store.threshold ? Color.app.blue1 : Color.app.gray5)
-								if option == store.recommended {
-									Text("Recommended")
+				GeometryReader { geo in
+					ScrollViewReader { scrollProxy in
+						ScrollView(.horizontal) {
+							LazyHGrid(rows: [GridItem(.fixed(150))], content: {
+								ForEach(store.options, id: \.self) { option in
+									Button.init(action: {
+										scrollProxy.scrollTo(option, anchor: .center)
+									}, label: {
+										VStack {
+											Text("\(option)").font(.largeTitle)
+												.fontWeight(.bold)
+												.foregroundStyle(option == store.threshold ? Color.app.blue1 : Color.app.gray5)
+											if option == store.recommended {
+												Text("Recommended")
+											}
+										}
+									})
+									.buttonStyle(.plain)
+									.id(option)
+									.frame(width: Self.cellWidth)
 								}
+							})
+							.padding(.leading, (geo.size.width - Self.cellWidth) / 2)
+							.background(
+								GeometryReader {
+									Color.clear.preference(
+										key: ViewOffsetKey.self,
+										value: -$0.frame(
+											in: .named("scroll")
+										)
+										.origin.x
+									)
+								}
+							)
+							.onPreferenceChange(ViewOffsetKey.self) {
+								send(.changedThreshold(Int($0 / Self.cellWidth)))
 							}
-							.frame(width: 70)
-						}
+						}.coordinateSpace(name: "scroll")
 					}
 				}
 				
@@ -82,6 +120,18 @@ extension SetFactorThresholdFeature {
 			.lineLimit(nil)
 			.padding()
 		}
+	}
+}
+
+struct ViewOffsetKey: PreferenceKey {
+	typealias Value = CGFloat
+	static let defaultValue = CGFloat.zero
+	
+	static func reduce(
+		value: inout Value,
+		nextValue: () -> Value
+	) {
+		value += nextValue()
 	}
 }
 
@@ -100,4 +150,18 @@ extension Role {
 		case .confirmation: return "confirm recovery"
 		}
 	}
+}
+
+#Preview {
+SetFactorThresholdFeature.View(
+	store: Store(
+		initialState: SetFactorThresholdFeature.State(
+			role: .primary,
+			numberOfFactors: 5,
+			threshold: .any
+		),
+	reducer: {
+		SetFactorThresholdFeature()
+	}
+))
 }
