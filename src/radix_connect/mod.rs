03c70322bddaecbfaabeb0532667f093b665852f
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2024-06-11 14:38:19 +0300

    Revert "wip"
    This reverts commit 3ea7600dcbb1658db3e5ec9c9f99f89a9eb0eedd.

--- src/radix_connect/mod.rs
+++ src/radix_connect/mod.rs
@@ -2,6 +2,7 @@ mod interaction_id;
 mod interaction_version;
 mod p2p_links;
 mod wallet_account;
+mod wallet_account_uniffi_fn;
 mod wallet_interaction;
 mod wallet_persona;
 
@@ -12,5 +13,6 @@ pub use interaction_id::*;
 pub use interaction_version::*;
 pub use p2p_links::*;
 pub use wallet_account::*;
+pub use wallet_account_uniffi_fn::*;
 pub use wallet_interaction::*;
 pub use wallet_persona::*;
