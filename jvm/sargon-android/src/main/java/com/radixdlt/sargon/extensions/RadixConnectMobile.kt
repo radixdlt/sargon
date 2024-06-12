44b2f5cb0f5af6d8b3ef8597c80dc9064b34e983
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-06-12 18:13:28 +0300

    Expose kotlin serde for RadixConnectMobileLinkRequest

---
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/RadixConnectMobile.kt
@@ -0,0 +1,17 @@
+package com.radixdlt.sargon.extensions
+
+import android.content.Context
+import com.radixdlt.sargon.RadixConnectMobile
+import com.radixdlt.sargon.annotation.KoverIgnore
+import com.radixdlt.sargon.antenna.SargonNetworkAntenna
+import com.radixdlt.sargon.os.radixconnect.RadixConnectSessionStorage
+import okhttp3.OkHttpClient
+
+@KoverIgnore
+fun RadixConnectMobile.Companion.init(
+    context: Context,
+    okHttpClient: OkHttpClient
+) = RadixConnectMobile(
+    networkAntenna = SargonNetworkAntenna(client = okHttpClient),
+    sessionStorage = RadixConnectSessionStorage(context = context)
+)
