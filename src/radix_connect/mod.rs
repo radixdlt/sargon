b464f396cf60d99672c6bdbce6ae950948df1bef
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2024-06-14 10:47:33 +0300

    export new_wallet_interaction_version_current

--- src/radix_connect/mod.rs
+++ src/radix_connect/mod.rs
@@ -1,5 +1,6 @@
 mod interaction_id;
 mod interaction_version;
+mod interaction_version_uniffi_fn;
 mod p2p_links;
 mod wallet_account;
 mod wallet_account_uniffi_fn;
@@ -11,6 +12,7 @@ mod mobile;
 
 pub use interaction_id::*;
 pub use interaction_version::*;
+pub use interaction_version_uniffi_fn::*;
 pub use p2p_links::*;
 pub use wallet_account::*;
 pub use wallet_account_uniffi_fn::*;
