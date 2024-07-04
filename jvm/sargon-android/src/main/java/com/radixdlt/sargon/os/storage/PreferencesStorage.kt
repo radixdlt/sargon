e187b29ced282ea0f9f653ed184be1a985423b11
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2024-07-04 15:27:09 +0300

    Expose home cards manager in kt

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/storage/PreferencesStorage.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/storage/PreferencesStorage.kt
@@ -3,30 +3,22 @@ package com.radixdlt.sargon.os.storage
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import com.radixdlt.sargon.extensions.then
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
-internal class EncryptedPreferencesStorage(
+internal class PreferencesStorage(
     private val datastore: DataStore<Preferences>
 ) {
 
-    suspend fun <T : Any> get(key: Preferences.Key<T>, keySpec: KeySpec): Result<T?> = runCatching {
+    suspend fun <T : Any> get(key: Preferences.Key<T>): Result<T?> = runCatching {
         datastore.data.catchIOException().map { preferences -> preferences[key] }.first()
-    }.then { encrypted ->
-        if (encrypted == null) return@then Result.success(null)
-
-        encrypted.decrypt(keySpec = keySpec)
     }
 
     suspend fun <T : Any> set(
         key: Preferences.Key<T>,
-        value: T,
-        keySpec: KeySpec
-    ): Result<Unit> = value.encrypt(keySpec = keySpec).mapCatching { encrypted ->
-        datastore.edit { preferences ->
-            preferences[key] = encrypted
-        }
+        value: T
+    ) = datastore.edit { preferences ->
+        preferences[key] = value
     }
 
     suspend fun <T : Any> remove(key: Preferences.Key<T>) {
