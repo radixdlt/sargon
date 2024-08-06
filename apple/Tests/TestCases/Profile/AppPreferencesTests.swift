fe0fdadf7d4c96dbc961db0f61d88de80af03caf
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-08-06 11:40:58 +0200

    Add Swift test

--- apple/Tests/TestCases/Profile/AppPreferencesTests.swift
+++ apple/Tests/TestCases/Profile/AppPreferencesTests.swift
@@ -17,8 +17,26 @@ final class AppPreferencesTests: Test<AppPreferences> {
 	}
 	
 	func test_has_gateway() {
-		XCTAssertTrue(SUT.default.hasGateway(with: .init(string: "https://mainnet.radixdlt.com/")!))
-		XCTAssertTrue(SUT.default.hasGateway(with: .init(string: "https://mainnet.radixdlt.com")!))
-		XCTAssertFalse(SUT.default.hasGateway(with: .init(string: "https://radixdlt.com")!))
+		// Valid URLs
+		do {
+			var result = try SUT.default.hasGateway(with: .init(string: "https://mainnet.radixdlt.com/")!)
+			XCTAssertTrue(result)
+			result = try SUT.default.hasGateway(with: .init(string: "https://mainnet.radixdlt.com")!)
+			XCTAssertTrue(result)
+			result = try SUT.default.hasGateway(with: .init(string: "https://radixdlt.com")!)
+			XCTAssertFalse(result)
+		} catch {
+			XCTFail("Unexpected failure")
+		}
+		
+		// Invalid URL
+		do {
+			let url = URL(string: "Rust considers this invalid")
+			XCTAssertNotNil(url)
+			let _ = try SUT.default.hasGateway(with: url!)
+			XCTFail("Should have thrown")
+		} catch let error {
+			XCTAssertEqual(error.localizedDescription, "Failed to convert arg \'url\': relative URL without a base")
+		}
 	}
 }
