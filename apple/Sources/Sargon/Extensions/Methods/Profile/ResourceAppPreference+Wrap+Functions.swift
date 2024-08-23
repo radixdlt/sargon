d2164fea886596993cea73d7a23caf3d4c89eec3
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-08-23 18:14:43 +0200

    fix iOS

---
+++ apple/Sources/Sargon/Extensions/Methods/Profile/ResourceAppPreference+Wrap+Functions.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matias Bzurovski on 13/8/24.
+//
+
+import Foundation
+import SargonUniFFI
+
+extension [ResourceAppPreference] {
+	public var hiddenResources: [ResourceIdentifier] {
+		resourcePreferencesGetHiddenResources(resourcePreferences: self)
+	}
+	
+	public mutating func hideResource(resource: ResourceIdentifier) {
+		self = resourcePreferencesHideResource(resourcePreferences: self, resource: resource)
+	}
+	
+	public mutating func unhideResource(resource: ResourceIdentifier) {
+		self = resourcePreferencesUnhideResource(resourcePreferences: self, resource: resource)
+	}
+}
---
+++ apple/Tests/TestCases/Profile/ResourcePreferencesTests.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matias Bzurovski on 13/8/24.
+//
+
+import CustomDump
+import Foundation
+import Sargon
+import SargonUniFFI
+import XCTest
+
+final class AssetPreferencesTests: TestCase {
+	func test_hidden_resources() {
+		var sut: [ResourceAppPreference] = []
+		XCTAssertTrue(sut.hiddenResources.isEmpty)
+		
+		// Hide assets
+		sut.hideResource(resource: .fungible(.sample))
+		sut.hideResource(resource: .nonFungible(.sample))
+		sut.hideResource(resource: .fungible(.sampleOther))
+		
+		XCTAssertEqual(sut.hiddenResources, [.fungible(.sample), .nonFungible(.sample), .fungible(.sampleOther)])
+		
+		// Unhide assets
+		sut.unhideResource(resource:  .fungible(.sampleOther))
+		sut.unhideResource(resource: .nonFungible(.sample))
+		XCTAssertEqual(sut.hiddenResources, [.fungible(.sample)])
+	}
+}
