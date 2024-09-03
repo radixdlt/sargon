1a265967d9fc90271608112a63416ed3210a1237
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-09-03 12:02:00 +0300

    Make profileStateChanges a shared flow

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/driver/AndroidProfileStateChangeDriver.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/driver/AndroidProfileStateChangeDriver.kt
@@ -4,22 +4,25 @@ import com.radixdlt.sargon.Profile
 import com.radixdlt.sargon.ProfileState
 import com.radixdlt.sargon.ProfileStateChangeDriver
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
 
 class AndroidProfileStateChangeDriver : ProfileStateChangeDriver {
 
-    private val _profileState = MutableStateFlow<ProfileState>(ProfileState.None)
+    private val _profileStateChanges = MutableSharedFlow<ProfileState>()
 
-    val profileState: StateFlow<ProfileState> = _profileState.asStateFlow()
+    val profileState: SharedFlow<ProfileState> = _profileStateChanges.asSharedFlow()
     val profile: Flow<Profile> = profileState.filterIsInstance<ProfileState.Loaded>().map {
         it.v1
     }
 
     override suspend fun handleProfileStateChange(changedProfileState: ProfileState) {
-        _profileState.emit(changedProfileState)
+        _profileStateChanges.emit(changedProfileState)
     }
 }
