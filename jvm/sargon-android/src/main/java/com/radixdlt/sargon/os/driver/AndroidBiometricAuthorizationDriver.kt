a54e5218920e870e4d7c92e37d8b8f694d6505d2
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-09-04 15:19:26 +0300

    Remove checks before asking for biometrics

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/driver/AndroidBiometricAuthorizationDriver.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/os/driver/AndroidBiometricAuthorizationDriver.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.radixdlt.sargon.annotation.KoverIgnore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.collectLatest
@@ -26,49 +25,11 @@ internal interface BiometricAuthorizationDriver {
 
 }
 
-sealed class BiometricsFailure(override val message: String?) : Exception() {
-
-    data class AuthenticationNotPossible(
-        val authenticationStatus: Int
-    ) : BiometricsFailure(
-        message = "Biometrics failed to request. canAuthenticate() returned [$authenticationStatus] ${authenticationStatus.toAuthenticationStatusMessage()}"
-    )
-
-    data class AuthenticationError(
-        val errorCode: Int,
-        val errorMessage: String
-    ) : BiometricsFailure(
-        message = "User did not authorize. Received [$errorCode]: $errorMessage"
-    )
-
-    companion object {
-        private fun Int.toAuthenticationStatusMessage(): String = when (this) {
-            BiometricManager.BIOMETRIC_SUCCESS ->
-                "The user can successfully authenticate."
-
-            BiometricManager.BIOMETRIC_STATUS_UNKNOWN ->
-                "Unable to determine whether the user can authenticate."
-
-            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED ->
-                "The user can't authenticate because the specified options are incompatible with the current Android version."
-
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
-                "The user can't authenticate because the hardware is unavailable. Try again later."
-
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
-                "The user can't authenticate because no biometric or device credential is enrolled."
-
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
-                "The user can't authenticate because there is no suitable hardware (e. g. no biometric sensor or no keyguard)."
-
-            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED ->
-                "The user can't authenticate because a security vulnerability has been discovered with one or more hardware sensors. The affected sensor(s) are unavailable until a security update has addressed the issue."
-
-            else -> ""
-        }
-    }
-
-}
+class BiometricsFailure(
+    @BiometricPrompt.AuthenticationError
+    val errorCode: Int,
+    val errorMessage: String?
+) : Exception("[$errorCode] $errorMessage")
 
 internal class AndroidBiometricAuthorizationDriver(
     private val biometricsHandler: BiometricsHandler
@@ -115,19 +76,6 @@ class BiometricsHandler(
         activity: FragmentActivity
     ): Result<Unit> = withContext(Dispatchers.Main) {
         suspendCoroutine { continuation ->
-            val biometricManager = BiometricManager.from(activity)
-
-            val authenticationPreCheckStatus =
-                biometricManager.canAuthenticate(allowedAuthenticators)
-            if (authenticationPreCheckStatus != BiometricManager.BIOMETRIC_SUCCESS) {
-                continuation.resume(
-                    Result.failure(
-                        BiometricsFailure.AuthenticationNotPossible(authenticationPreCheckStatus)
-                    )
-                )
-                return@suspendCoroutine
-            }
-
             val authCallback = object : BiometricPrompt.AuthenticationCallback() {
                 override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                     continuation.resume(Result.success(Unit))
@@ -135,12 +83,7 @@ class BiometricsHandler(
 
                 override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
                     continuation.resume(
-                        Result.failure(
-                            BiometricsFailure.AuthenticationError(
-                                errorCode,
-                                errString.toString()
-                            )
-                        )
+                        Result.failure(BiometricsFailure(errorCode, errString.toString()))
                     )
                 }
 
