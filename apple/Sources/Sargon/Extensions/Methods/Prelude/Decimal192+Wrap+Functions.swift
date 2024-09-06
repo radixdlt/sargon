7fe72d6f135e317eb1bb39155d762239a7f4c64d
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-09-06 15:42:32 +0200

    moving to Swiftified extension

--- apple/Sources/Sargon/Extensions/Methods/Prelude/Decimal192+Wrap+Functions.swift
+++ apple/Sources/Sargon/Extensions/Methods/Prelude/Decimal192+Wrap+Functions.swift
@@ -73,17 +73,6 @@ extension Decimal192 {
 	public static let maxDivisibility: UInt8 = 18
     
     public static let temporaryStandardFee: Self = transactionFeePreset()
-	
-	public static let one: Self 	= 1
-	public static let two: Self 	= 2
-	public static let three: Self 	= 3
-	public static let four: Self 	= 4
-	public static let five: Self 	= 5
-	public static let six: Self 	= 6
-	public static let seven: Self = 7
-	public static let eight: Self = 8
-	public static let nine: Self 	= 9
-	public static let ten: Self 	= 10
 }
 
 extension Decimal192 {
