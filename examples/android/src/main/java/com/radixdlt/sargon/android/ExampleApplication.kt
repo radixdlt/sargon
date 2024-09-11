e488911c597fd2d71eedf89cc23ca68a4e22b9e7
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-09-11 11:59:41 +0300

    Change doc

--- examples/android/src/main/java/com/radixdlt/sargon/android/ExampleApplication.kt
+++ examples/android/src/main/java/com/radixdlt/sargon/android/ExampleApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class ExampleApplication: Application() {
+class ExampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
