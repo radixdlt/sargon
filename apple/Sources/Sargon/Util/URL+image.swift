87e8182d7d5009ca69b76ea8a8f022f968b751d6
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2024-09-25 15:29:51 +0300

    wip

---
+++ apple/Sources/Sargon/Util/URL+image.swift
@@ -0,0 +1,17 @@
+import Foundation
+import SargonUniFFI
+
+extension URL {
+	public func isVectorImage(type: VectorImageType) -> Bool {
+		imageUrlUtilsIsVectorImage(url: self, imageType: type)
+	}
+
+	public func imageURL(imageServiceURL: URL, size: CGSize) throws -> URL {
+		try imageUrlUtilsMakeImageUrl(
+			url: self.absoluteString,
+			imageServiceUrl: imageServiceURL.absoluteString,
+			width: UInt32(size.width),
+			height: UInt32(size.height)
+		)
+	}
+}
