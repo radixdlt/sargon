909af4227721d647599a30e71346aec5061c698e
Alexander Cyon <alex.cyon@rdx.works>
2024-09-26 16:24:14 +0200

    Revert "fix tests"
    This reverts commit 79ce9798de254fafdf9c0155cec3d514deb92484.

--- apple/Tests/IntegrationTests/GatewayClientTests.swift
+++ apple/Tests/IntegrationTests/GatewayClientTests.swift
@@ -5,56 +5,62 @@ import SargonUniFFI
 import XCTest
 
 final class GatewayClientTests: TestCase {
-    func test_xrd_balance_of_unknown_stokenet_account_is_zero() async throws {
-        let gateway = GatewayClient(networkID: .stokenet)
-        let xrdBalance = try await gateway.xrdBalanceOfAccountOrZero(address: AccountAddress.random(networkID: .stokenet))
-        XCTAssertEqual(xrdBalance, 0)
-    }
+	
+	func test_xrd_balance_of_account() async throws {
+		let gateway = GatewayClient(networkID: .mainnet)
+		let xrdBalance = try await gateway.xrdBalanceOfAccountOrZero(address: .sample)
+		XCTAssertGreaterThan(xrdBalance, 1)
+	}
+	
+	
 }
 
 final class NetworkResponseTests: TestCase {
-    typealias SUT = NetworkResponse
-    func test_response_not_http_url() throws {
-        XCTAssertThrowsError(
-            try SUT(
-                response: (Data(), URLResponse())
-            )
-        )
-    }
+	typealias SUT = NetworkResponse
+	func test_response_not_http_url() throws {
+		XCTAssertThrowsError(
+			try SUT(
+				response: (Data(), URLResponse())
+			)
+		)
+	}
 }
 
 final class NetworkAntennaTests: TestCase {
-    func test_url_session_executeNetworkRequest_bad_response() async throws {
-        struct Fail: Swift.Error {}
+	func test_url_session_executeNetworkRequest_bad_response() async throws {
+		
+		struct Fail: Swift.Error {}
+	
+		class MockURLProtocol: URLProtocol {
+			
+			override class func canInit(with request: URLRequest) -> Bool {
+				true
+			}
+			
+			override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+				request
+			}
+			
+			override func startLoading() {
+				self.client?.urlProtocol(self, didFailWithError: Fail())
+			}
+			
+			override func stopLoading() {}
+		}
+		
+		let failURLSession: URLSession = {
+			let configuration = URLSessionConfiguration.ephemeral
+			configuration.protocolClasses = [MockURLProtocol.self]
+			return URLSession(configuration: configuration)
+		}()
+		
 
-        class MockURLProtocol: URLProtocol {
-            override class func canInit(with _: URLRequest) -> Bool {
-                true
-            }
-
-            override class func canonicalRequest(for request: URLRequest) -> URLRequest {
-                request
-            }
-
-            override func startLoading() {
-                client?.urlProtocol(self, didFailWithError: Fail())
-            }
-
-            override func stopLoading() {}
-        }
-
-        let failURLSession: URLSession = {
-            let configuration = URLSessionConfiguration.ephemeral
-            configuration.protocolClasses = [MockURLProtocol.self]
-            return URLSession(configuration: configuration)
-        }()
-
-        let failGateway = GatewayClient(networkingDriver: failURLSession, networkId: .mainnet)
-        do {
-            _ = try await failGateway.xrdBalanceOfAccountOrZero(address: AccountAddress.sample)
-            XCTFail("Expected to fail")
-        } catch {
-            // Good expected to fail!
-        }
-    }
+		let failGateway = GatewayClient(networkingDriver: failURLSession, networkId: .mainnet)
+		do {
+			_ = try await failGateway.xrdBalanceOfAccountOrZero(address: AccountAddress.sample)
+			XCTFail("Expected to fail")
+		} catch {
+			// Good expected to fail!
+		}
+	}
 }
--- crates/sargon/tests/integration/main.rs
+++ crates/sargon/tests/integration/main.rs
@@ -17,8 +17,18 @@ mod integration_tests {
     }
 
     #[actix_rt::test]
+    async fn test_xrd_balance_of_account_or_zero() {
+        let gateway_client = new_gateway_client(NetworkID::Mainnet);
+        let sut = gateway_client
+            .xrd_balance_of_account_or_zero(AccountAddress::sample_mainnet());
+
+        let xrd_balance = timeout(MAX, sut).await.unwrap().unwrap();
+        assert!(xrd_balance >= Decimal192::one())
+    }
+
+    #[actix_rt::test]
     async fn test_xrd_balance_of_account_or_zero_is_zero_for_unknown_mainnet() {
-        let network_id = NetworkID::Stokenet;
+        let network_id = NetworkID::Mainnet;
         let gateway_client = new_gateway_client(network_id);
         let sut = gateway_client
             .xrd_balance_of_account_or_zero(AccountAddress::random(network_id));
@@ -41,7 +51,7 @@ mod integration_tests {
 
     #[actix_rt::test]
     async fn test_epoch() {
-        let gateway_client = new_gateway_client(NetworkID::Stokenet);
+        let gateway_client = new_gateway_client(NetworkID::Mainnet);
         let sut = gateway_client.current_epoch();
         let epoch = timeout(MAX, sut).await.unwrap().unwrap();
         assert!(epoch > Epoch::from(0));
@@ -50,7 +60,7 @@ mod integration_tests {
     #[actix_rt::test]
     async fn dry_run_transaction() {
         // ARRANGE
-        let network_id = NetworkID::Stokenet;
+        let network_id = NetworkID::Mainnet;
         let gateway_client = new_gateway_client(network_id);
         let start_epoch_inclusive =
             timeout(MAX, gateway_client.current_epoch())
