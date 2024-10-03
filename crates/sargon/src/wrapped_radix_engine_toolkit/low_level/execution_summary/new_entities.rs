b61fdd81382551d9e60ab5042d587c659f06106e
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-10-03 16:53:23 +0300

    For format

--- crates/sargon/src/wrapped_radix_engine_toolkit/low_level/execution_summary/new_entities.rs
+++ crates/sargon/src/wrapped_radix_engine_toolkit/low_level/execution_summary/new_entities.rs
@@ -23,21 +23,18 @@ impl From<(RetNewEntities, NetworkID)> for NewEntities {
     fn from(value: (RetNewEntities, NetworkID)) -> Self {
         let (ret, network_id) = value;
 
-        Self::new(
-            ret.resource_addresses.into_iter().map(|r| {
-                let resource_address = ResourceAddress::from((r, network_id));
-                let global_address =
-                    ScryptoGlobalAddress::from(resource_address);
+        Self::new(ret.resource_addresses.into_iter().map(|r| {
+            let resource_address = ResourceAddress::from((r, network_id));
+            let global_address = ScryptoGlobalAddress::from(resource_address);
 
-                let newly_created_resource = ret
-                    .metadata
-                    .get(&global_address)
-                    .map(|m| NewlyCreatedResource::from(m.clone()))
-                    .unwrap_or_default();
+            let newly_created_resource = ret
+                .metadata
+                .get(&global_address)
+                .map(|m| NewlyCreatedResource::from(m.clone()))
+                .unwrap_or_default();
 
-                (resource_address, newly_created_resource)
-            })
-        )
+            (resource_address, newly_created_resource)
+        }))
     }
 }
 
