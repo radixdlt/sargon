e3a1e77ab9940f510a65bd28de7dc3b98c718b4f
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-10-08 15:44:17 +0300

    Add securified variant in EntitySecurityState

--- crates/sargon/src/signing/petition_types/mod.rs
+++ crates/sargon/src/signing/petition_types/mod.rs
@@ -2,8 +2,11 @@ mod petition_for_factors_types;
 mod factor_list_kind;
 mod general_role_with_hd_factor_instance;
 mod role_kind;
+mod petition_for_entity;
 
 pub(crate) use factor_list_kind::*;
+pub(crate) use petition_for_entity::*;
 pub(crate) use role_kind::*;
+pub(crate) use general_role_with_hd_factor_instance::*;
 
 pub use petition_for_factors_types::*;
