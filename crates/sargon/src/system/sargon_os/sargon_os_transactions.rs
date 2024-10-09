20ad856800132272a9a2c14747f7325690e92ff2
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-10-09 16:23:36 +0200

    test the delay among polls

--- crates/sargon/src/system/sargon_os/sargon_os_transactions.rs
+++ crates/sargon/src/system/sargon_os/sargon_os_transactions.rs
@@ -23,7 +23,7 @@ impl SargonOS {
 }
 
 // ==================
-// Poll Transaction Status
+// Poll Transaction Status (Public)
 // ==================
 #[uniffi::export]
 impl SargonOS {
@@ -32,25 +32,46 @@ impl SargonOS {
         &self,
         intent_hash: IntentHash,
     ) -> Result<TransactionStatus> {
+        let (status, _) = self
+            .poll_transaction_status_with_delays(intent_hash)
+            .await?;
+        Ok(status)
+    }
+}
+
+// ==================
+// Poll Transaction Status (Internal)
+// ==================
+impl SargonOS {
+    /// Polls the state of a Transaction until we can determine its `TransactionStatus`.
+    ///
+    /// This is the internal implementation of `poll_transaction_status`, which is the public API.
+    /// It returns the `TransactionStatus`, but also the list of delays between each poll.
+    async fn poll_transaction_status_with_delays(
+        &self,
+        intent_hash: IntentHash,
+    ) -> Result<(TransactionStatus, Vec<u64>)> {
         let network_id = self.current_network_id()?;
         let gateway_client = GatewayClient::new(
             self.clients.http_client.driver.clone(),
             network_id,
         );
+        let mut delays: Vec<u64> = vec![];
 
-        // The delay increment is set to 1 second in production, but 0 seconds in tests.
-        // This will make the tests run with 0s delay, while the production code will have a 2s delay on first call,
-        // a 3s delay on second call, 4s on third and so on.
+        // The delay increment is set to 1 second in production, but 1 millisecond in tests.
+        // This will make the tests run with almost no delay, while the production code will have a 2s delay after first call,
+        // a 3s delay after second call, 4s after third and so on.
         #[cfg(test)]
-        const DELAY_INCREMENT: u64 = 0;
-        #[cfg(not(test))]
         const DELAY_INCREMENT: u64 = 1;
+        #[cfg(not(test))]
+        const DELAY_INCREMENT: u64 = 1000;
 
         let mut delay_duration = DELAY_INCREMENT;
+
         loop {
             // Increase delay by 1 second on subsequent calls
             delay_duration += DELAY_INCREMENT;
-            let sleep_duration = Duration::from_secs(delay_duration);
+            let sleep_duration = Duration::from_millis(delay_duration);
 
             let response = gateway_client
                 .get_transaction_status(intent_hash.clone())
@@ -66,23 +87,25 @@ impl SargonOS {
                         TransactionStatusResponsePayloadStatus::Unknown |
                         TransactionStatusResponsePayloadStatus::Pending |
                         TransactionStatusResponsePayloadStatus::CommitPendingOutcomeUnknown => {
+                            delays.push(delay_duration);
                             async_std::task::sleep(sleep_duration).await;
                         }
                         TransactionStatusResponsePayloadStatus::CommittedSuccess => {
-                            return Ok(TransactionStatus::Success);
+                            return Ok((TransactionStatus::Success, delays));
                         }
                         TransactionStatusResponsePayloadStatus::CommittedFailure => {
-                            return Ok(TransactionStatus::Failed { reason: TransactionStatusReason::from_raw_error(response.error_message) });
+                            return Ok((TransactionStatus::Failed { reason: TransactionStatusReason::from_raw_error(response.error_message) }, delays));
                         }
                         TransactionStatusResponsePayloadStatus::PermanentlyRejected => {
-                            return Ok(TransactionStatus::PermanentlyRejected { reason: TransactionStatusReason::from_raw_error(response.error_message) });
+                            return Ok((TransactionStatus::PermanentlyRejected { reason: TransactionStatusReason::from_raw_error(response.error_message) }, delays));
                         }
                         TransactionStatusResponsePayloadStatus::TemporarilyRejected => {
-                            return Ok(TransactionStatus::TemporarilyRejected { current_epoch: Epoch::from(response.ledger_state.epoch) });
+                            return Ok((TransactionStatus::TemporarilyRejected { current_epoch: Epoch::from(response.ledger_state.epoch) }, delays));
                         }
                     }
                 }
                 None => {
+                    delays.push(delay_duration);
                     async_std::task::sleep(sleep_duration).await;
                 }
             }
@@ -172,25 +195,32 @@ mod poll_status_tests {
         let result =
             simulate_poll_status(vec![sample_committed_success()]).await;
 
-        assert_eq!(result, TransactionStatus::Success);
+        // Result should be `Success`
+        assert_eq!(result.0, TransactionStatus::Success);
+        // and there shouldn't be any delays
+        assert!(result.1.is_empty());
     }
 
     #[actix_rt::test]
-    async fn poll_status_empty_then_failure() {
+    async fn poll_status_pending_then_failure() {
         // This test will simulate the case where the first response is empty (no payload status),
-        // while the second response is a `CommittedFailure`
+        // the second is `Pending` and the third is a `CommittedFailure`
         let result = simulate_poll_status(vec![
+            sample_empty(),
             sample_pending(),
             sample_committed_failure(None),
         ])
         .await;
 
+        // Result should be `Failed`
         assert_eq!(
-            result,
+            result.0,
             TransactionStatus::Failed {
                 reason: TransactionStatusReason::Unknown
             }
         );
+        // and there should have been a delay of 2s after first call, and 3s after the second call
+        assert_eq!(result.1, vec![2, 3]);
     }
 
     #[actix_rt::test]
@@ -203,12 +233,15 @@ mod poll_status_tests {
         ])
         .await;
 
+        // Result should be `PermanentlyRejected`
         assert_eq!(
-            result,
+            result.0,
             TransactionStatus::PermanentlyRejected {
                 reason: TransactionStatusReason::WorktopError
             }
         );
+        // and there should have been a delay of 2s after first call
+        assert_eq!(result.1, vec![2]);
     }
 
     #[actix_rt::test]
@@ -223,10 +256,13 @@ mod poll_status_tests {
         .await;
 
         let current_epoch = Epoch::from(LedgerState::sample_stokenet().epoch);
+        // Result should be `TemporarilyRejected`
         assert_eq!(
-            result,
+            result.0,
             TransactionStatus::TemporarilyRejected { current_epoch }
         );
+        // and there should have been a delay of 2s after first call
+        assert_eq!(result.1, vec![2]);
     }
 
     #[actix_rt::test]
@@ -254,7 +290,7 @@ mod poll_status_tests {
     // and then call `poll_transaction_status` to get the result.
     async fn simulate_poll_status(
         responses: Vec<TransactionStatusResponse>,
-    ) -> TransactionStatus {
+    ) -> (TransactionStatus, Vec<u64>) {
         let mock_driver = MockNetworkingDriver::with_responses(responses);
 
         let req = SUT::boot_test_with_networking_driver(Arc::new(mock_driver));
@@ -265,12 +301,20 @@ mod poll_status_tests {
                 .unwrap()
                 .unwrap();
 
-        os.poll_transaction_status(IntentHash::sample())
+        os.poll_transaction_status_with_delays(IntentHash::sample())
             .await
             .unwrap()
     }
 
     // Helper functions to create sample responses
+
+    fn sample_empty() -> TransactionStatusResponse {
+        TransactionStatusResponse {
+            known_payloads: vec![],
+            ledger_state: LedgerState::sample_stokenet(),
+            error_message: None,
+        }
+    }
 
     fn sample_unknown() -> TransactionStatusResponse {
         TransactionStatusResponse {
