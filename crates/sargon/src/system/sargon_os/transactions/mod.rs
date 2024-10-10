74044f56911ff7ff91d54cedea7c65479e8605dd
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-10-10 15:03:00 +0200

    Split tx submit and poll status in two files

--- crates/sargon/src/system/sargon_os/transactions/mod.rs
+++ crates/sargon/src/system/sargon_os/transactions/mod.rs
@@ -1,7 +1,9 @@
 mod sargon_os_transaction_analysis;
-mod sargon_os_transactions;
+mod sargon_os_transaction_status;
+mod sargon_os_transaction_submit;
 mod support;
 
 pub use sargon_os_transaction_analysis::*;
-pub use sargon_os_transactions::*;
+pub use sargon_os_transaction_status::*;
+pub use sargon_os_transaction_submit::*;
 pub use support::*;
