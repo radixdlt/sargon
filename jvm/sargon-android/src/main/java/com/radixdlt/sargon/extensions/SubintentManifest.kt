76aa4b3d53d5ef2cef5b1820c9fe0b35a77a1aea
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2024-10-25 11:37:11 +0300

    fix

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/SubintentManifest.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/SubintentManifest.kt
@@ -1,17 +1,13 @@
 package com.radixdlt.sargon.extensions
 
 import com.radixdlt.sargon.Blobs
-import com.radixdlt.sargon.ManifestSummary
-import com.radixdlt.sargon.NetworkId
 import com.radixdlt.sargon.PoolAddress
 import com.radixdlt.sargon.ResourceAddress
 import com.radixdlt.sargon.SubintentManifest
 import com.radixdlt.sargon.subintentManifestBlobs
 import com.radixdlt.sargon.subintentManifestInvolvedPoolAddresses
 import com.radixdlt.sargon.subintentManifestInvolvedResourceAddresses
-import com.radixdlt.sargon.subintentManifestNetworkId
 import com.radixdlt.sargon.subintentManifestString
-import com.radixdlt.sargon.subintentManifestSummary
 
 val SubintentManifest.manifestString: String
     get() = subintentManifestString(manifest = this)
