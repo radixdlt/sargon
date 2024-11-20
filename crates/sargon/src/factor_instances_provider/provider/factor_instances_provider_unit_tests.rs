3bc824d87e62b0a0eee872abd00b15c155d2c542
Alexander Cyon <alex.cyon@gmail.com>
2024-11-20 10:47:39 +0100

    test all roles

--- crates/sargon/src/factor_instances_provider/provider/factor_instances_provider_unit_tests.rs
+++ crates/sargon/src/factor_instances_provider/provider/factor_instances_provider_unit_tests.rs
@@ -1414,6 +1414,23 @@ async fn securify_accounts_when_cache_is_half_full_multiple_factor_sources() {
         !derivation_outcome.derived_any_new_instance_for_any_factor_source(),
         "should have used cache"
     );
+    let expected = vec![
+        ["0^", "0^", "0^"],
+        ["1^", "1^", "1^"],
+        ["2^", "2^", "2^"],
+        ["3^", "3^", "3^"],
+        ["4^", "4^", "4^"],
+        ["5^", "5^", "5^"],
+        ["6^", "6^", "6^"],
+        ["7^", "7^", "7^"],
+        ["8^", "8^", "8^"],
+        ["9^", "9^", "9^"],
+        ["10^", "10^", "10^"],
+        ["11^", "11^", "11^"],
+        ["12^", "12^", "12^"],
+        ["13^", "13^", "13^"],
+        ["14^", "14^", "14^"],
+    ];
 
     assert_eq!(
         security_structures_of_fis
@@ -1427,23 +1444,37 @@ async fn securify_accounts_when_cache_is_half_full_multiple_factor_sources() {
                 .map(|x| format!("{:?}", x))
                 .collect_vec())
             .collect_vec(),
-        vec![
-            ["0^", "0^", "0^"],
-            ["1^", "1^", "1^"],
-            ["2^", "2^", "2^"],
-            ["3^", "3^", "3^"],
-            ["4^", "4^", "4^"],
-            ["5^", "5^", "5^"],
-            ["6^", "6^", "6^"],
-            ["7^", "7^", "7^"],
-            ["8^", "8^", "8^"],
-            ["9^", "9^", "9^"],
-            ["10^", "10^", "10^"],
-            ["11^", "11^", "11^"],
-            ["12^", "12^", "12^"],
-            ["13^", "13^", "13^"],
-            ["14^", "14^", "14^"]
-        ]
+        expected.clone()
+    );
+
+    assert_eq!(
+        security_structures_of_fis
+            .values()
+            .map(|ss| ss
+                .matrix_of_factors
+                .confirmation_role
+                .all_hd_factors()
+                .iter()
+                .map(|f| f.derivation_entity_index())
+                .map(|x| format!("{:?}", x))
+                .collect_vec())
+            .collect_vec(),
+        expected.clone()
+    );
+
+    assert_eq!(
+        security_structures_of_fis
+            .values()
+            .map(|ss| ss
+                .matrix_of_factors
+                .recovery_role
+                .all_hd_factors()
+                .iter()
+                .map(|f| f.derivation_entity_index())
+                .map(|x| format!("{:?}", x))
+                .collect_vec())
+            .collect_vec(),
+        expected.clone()
     );
 
     let (security_structures_of_fis, instances_in_cache_consumer, derivation_outcome) = os
@@ -1467,6 +1498,39 @@ async fn securify_accounts_when_cache_is_half_full_multiple_factor_sources() {
         "should have found some in cache"
     );
 
+    let expected = vec![
+        ["15^", "15^", "15^"],
+        ["16^", "16^", "16^"],
+        ["17^", "17^", "17^"],
+        ["18^", "18^", "18^"],
+        ["19^", "19^", "19^"],
+        ["20^", "20^", "20^"],
+        ["21^", "21^", "21^"],
+        ["22^", "22^", "22^"],
+        ["23^", "23^", "23^"],
+        ["24^", "24^", "24^"],
+        ["25^", "25^", "25^"],
+        ["26^", "26^", "26^"],
+        ["27^", "27^", "27^"],
+        ["28^", "28^", "28^"],
+        ["29^", "29^", "29^"],
+        ["30^", "30^", "30^"],
+        ["31^", "31^", "31^"],
+        ["32^", "32^", "32^"],
+        ["33^", "33^", "33^"],
+        ["34^", "34^", "34^"],
+        ["35^", "35^", "35^"],
+        ["36^", "36^", "36^"],
+        ["37^", "37^", "37^"],
+        ["38^", "38^", "38^"],
+        ["39^", "39^", "39^"],
+        ["40^", "40^", "40^"],
+        ["41^", "41^", "41^"],
+        ["42^", "42^", "42^"],
+        ["43^", "43^", "43^"],
+        ["44^", "44^", "44^"],
+    ];
+
     assert_eq!(
         security_structures_of_fis
             .values()
@@ -1479,38 +1543,37 @@ async fn securify_accounts_when_cache_is_half_full_multiple_factor_sources() {
                 .map(|x| format!("{:?}", x))
                 .collect_vec())
             .collect_vec(),
-        vec![
-            ["15^", "15^", "15^"],
-            ["16^", "16^", "16^"],
-            ["17^", "17^", "17^"],
-            ["18^", "18^", "18^"],
-            ["19^", "19^", "19^"],
-            ["20^", "20^", "20^"],
-            ["21^", "21^", "21^"],
-            ["22^", "22^", "22^"],
-            ["23^", "23^", "23^"],
-            ["24^", "24^", "24^"],
-            ["25^", "25^", "25^"],
-            ["26^", "26^", "26^"],
-            ["27^", "27^", "27^"],
-            ["28^", "28^", "28^"],
-            ["29^", "29^", "29^"],
-            ["30^", "30^", "30^"],
-            ["31^", "31^", "31^"],
-            ["32^", "32^", "32^"],
-            ["33^", "33^", "33^"],
-            ["34^", "34^", "34^"],
-            ["35^", "35^", "35^"],
-            ["36^", "36^", "36^"],
-            ["37^", "37^", "37^"],
-            ["38^", "38^", "38^"],
-            ["39^", "39^", "39^"],
-            ["40^", "40^", "40^"],
-            ["41^", "41^", "41^"],
-            ["42^", "42^", "42^"],
-            ["43^", "43^", "43^"],
-            ["44^", "44^", "44^"]
-        ]
+        expected
+    );
+
+    assert_eq!(
+        security_structures_of_fis
+            .values()
+            .map(|ss| ss
+                .matrix_of_factors
+                .recovery_role
+                .all_hd_factors()
+                .iter()
+                .map(|f| f.derivation_entity_index())
+                .map(|x| format!("{:?}", x))
+                .collect_vec())
+            .collect_vec(),
+        expected
+    );
+
+    assert_eq!(
+        security_structures_of_fis
+            .values()
+            .map(|ss| ss
+                .matrix_of_factors
+                .confirmation_role
+                .all_hd_factors()
+                .iter()
+                .map(|f| f.derivation_entity_index())
+                .map(|x| format!("{:?}", x))
+                .collect_vec())
+            .collect_vec(),
+        expected
     );
 }
 
