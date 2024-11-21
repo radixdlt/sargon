b260fbc22f182a3652041048b8f8fa8b04ebdf56
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2024-11-21 10:17:27 +0200

    fmt

--- crates/sargon-uniffi/src/core/utils/mod.rs
+++ crates/sargon-uniffi/src/core/utils/mod.rs
@@ -1,7 +1,7 @@
+mod constants;
 mod conversion_tests_macro;
 mod delegate_debug_display_impl;
 mod image_url_utils_uniffi_fn;
-mod constants;
 
 pub use conversion_tests_macro::*;
 pub use delegate_debug_display_impl::*;
