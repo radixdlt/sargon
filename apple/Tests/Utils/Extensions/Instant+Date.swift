0da73b9704702fe4fab3bbd05d089a4534bf6cd4
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-11-28 16:40:08 +0100

    Swift extension

---
+++ apple/Tests/Utils/Extensions/Instant+Date.swift
@@ -0,0 +1,18 @@
+import Foundation
+import SargonUniFFI
+import XCTest
+
+final class InstantTests: TestCase {
+	typealias SUT = Instant
+	
+	func testDate() throws {
+		var sut = SUT.init(secondsSinceUnixEpoch: 0)
+		XCTAssertEqual(sut.date, Date(timeIntervalSince1970: 0))
+		
+		sut = .init(secondsSinceUnixEpoch: 500)
+		XCTAssertEqual(sut.date, Date(timeIntervalSince1970: 500))
+		
+		sut = .init(secondsSinceUnixEpoch: 1000)
+		XCTAssertEqual(sut.date.timeIntervalSince1970, 1000)
+	}
+}
