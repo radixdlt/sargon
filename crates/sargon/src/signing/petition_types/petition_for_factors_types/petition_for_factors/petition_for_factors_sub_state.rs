35c1e48f267005c7c3ee6f3a32b44a86a0b63fc2
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-11-29 16:45:37 +0200

    Replace with rwlock in PetitionForFactorsSubState

--- crates/sargon/src/signing/petition_types/petition_for_factors_types/petition_for_factors/petition_for_factors_sub_state.rs
+++ crates/sargon/src/signing/petition_types/petition_for_factors_types/petition_for_factors/petition_for_factors_sub_state.rs
@@ -2,29 +2,54 @@ use crate::prelude::*;
 
 /// A sub-state of `PetitionForFactorsState` which can be used to track factors
 /// that have signed or skipped.
-#[derive(Clone, PartialEq, Eq, derive_more::Debug)]
-#[debug("[{}]", factors.borrow().clone().into_iter().map(|f| format!("{:?}", f)).join(", "))]
+#[derive(derive_more::Debug)]
+#[debug("[{}]", factors.read().unwrap().clone().into_iter().map(|f| format!("{:?}", f)).join(", "))]
 pub(crate) struct PetitionForFactorsSubState<F>
 where
     F: FactorSourceReferencing + Debug,
 {
     /// Factors that have signed or skipped
-    factors: RefCell<IndexSet<F>>,
+    factors: RwLock<IndexSet<F>>,
 }
+
+// TODO remove
+impl <F: FactorSourceReferencing + Debug> Clone for PetitionForFactorsSubState<F> {
+    fn clone(&self) -> Self {
+        self.cloned()
+    }
+}
+
+impl <F: FactorSourceReferencing + Debug> PartialEq for PetitionForFactorsSubState<F> {
+    fn eq(&self, other: &Self) -> bool {
+        let self_state = self.snapshot();
+        let other_state = other.snapshot();
+
+        self_state == other_state
+    }
+}
+
+impl <F: FactorSourceReferencing + Debug> Eq for PetitionForFactorsSubState<F> {}
 
 impl<F: FactorSourceReferencing + Debug> PetitionForFactorsSubState<F> {
     pub(super) fn new() -> Self {
         Self {
-            factors: RefCell::new(IndexSet::new()),
+            factors: RwLock::new(IndexSet::new()),
         }
     }
 
     pub(super) fn insert(&self, factor: &F) {
-        self.factors.borrow_mut().insert(factor.clone());
+        self.factors
+            .write()
+            .expect("PetitionForFactorsSubState was poisoned")
+            .insert(factor.clone());
     }
 
     pub(super) fn snapshot(&self) -> IndexSet<F> {
-        self.factors.borrow().clone()
+        self
+            .factors
+            .read()
+            .expect("PetitionForFactorsSubState was poisoned")
+            .clone()
     }
 
     pub(super) fn references_factor_source_by_id(
@@ -32,8 +57,15 @@ impl<F: FactorSourceReferencing + Debug> PetitionForFactorsSubState<F> {
         factor_source_id: FactorSourceIDFromHash,
     ) -> bool {
         self.factors
-            .borrow()
+            .read()
+            .expect("PetitionForFactorsSubState was poisoned")
             .iter()
             .any(|sf| sf.factor_source_id() == factor_source_id)
+    }
+
+    pub(super) fn cloned(&self) -> Self {
+        Self {
+            factors: RwLock::new(self.snapshot()),
+        }
     }
 }
