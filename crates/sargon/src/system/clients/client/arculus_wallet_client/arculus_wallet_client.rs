283a0a2dddcf2b98bba71762f91e937ea3ce0f17
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2024-12-04 09:27:45 +0200

    wip

--- crates/sargon/src/system/clients/client/arculus_wallet_client/arculus_wallet_client.rs
+++ crates/sargon/src/system/clients/client/arculus_wallet_client/arculus_wallet_client.rs
@@ -1,5 +1,6 @@
 use std::future::Future;
 
+use indexmap::IndexSet;
 use radix_engine_interface::freeze_roles;
 
 pub use crate::prelude::*;
@@ -65,19 +66,65 @@ impl ArculusWalletClient {
     }
 
     pub async fn read_card_factor_source_id(&self) -> Result<FactorSourceIDFromHash> {
-        let gguid_raw = self.execute_card_operation(
-            |wallet| self.get_gguid_io(wallet)
-        ).await?;
-
-        let bytes = Exactly32Bytes::try_from(gguid_raw)?;
-
-        Ok(FactorSourceIDFromHash::new(FactorSourceKind::ArculusCard, bytes))
+        self.execute_card_operation(|wallet| 
+            self._read_card_factor_source_id(wallet)
+        ).await
     }
 
     pub async fn create_wallet_seed(&self, pin: String, word_count: u8) -> Result<Mnemonic> {
         self.execute_card_operation(|wallet| {
            self._create_wallet_seed(wallet, pin, word_count)
         }).await
+    }
+
+    pub async fn restore_wallet_seed(&self, mnemonic: Mnemonic, pin: String) -> Result<()> {
+        self.execute_card_operation(|wallet|
+            self._restore_wallet_seed(wallet, mnemonic, pin)
+        ).await
+    }
+
+    pub async fn derive_public_keys(&self, paths: IndexSet<DerivationPath>) -> Result<IndexSet<HierarchicalDeterministicPublicKey>> {
+        self.execute_card_operation(|wallet|
+            self._derive_public_keys(wallet, paths)
+        ).await
+    }
+}
+
+impl ArculusWalletClient {
+    pub async fn _derive_public_keys(&self, wallet: ArculusWalletPointer, paths: IndexSet<DerivationPath>) -> Result<IndexSet<HierarchicalDeterministicPublicKey>> {
+        let mut keys = IndexSet::new();
+
+    for path in paths {
+        // Each task starts only after the previous one completes
+        let result = self.get_public_key_by_path_io(wallet, path.clone()).await?;
+        let public_key = PublicKey::ed25519_from_bytes(&result)?;
+        let key = HierarchicalDeterministicPublicKey::new(public_key, path);
+        keys.insert(key);
+    }
+
+        Ok(keys)
+    }
+
+    pub async fn _restore_wallet_seed(&self, wallet: ArculusWalletPointer, mnemonic: Mnemonic, pin: String) -> Result<()> {
+        let pin_len = pin.len() as u8;
+        let seed_length = mnemonic.word_count.discriminant();
+
+        self.reset_wallet_io(wallet).await?;
+        self.store_pin_io(wallet, pin.clone(), pin_len).await?;
+        self.init_recover_wallet_io(wallet, seed_length).await?;
+        let seed = self.csdk_driver.seed_phrase_from_mnemonic_sentence(wallet,  mnemonic.phrase().as_bytes().into(), seed_length, None, 0)?;
+        self.finish_recover_wallet_io(wallet, seed, seed_length).await?;
+        self.verify_pin_io(wallet, pin, pin_len).await?;
+
+        Ok(())
+    }
+
+    async fn _read_card_factor_source_id(&self, wallet: ArculusWalletPointer) -> Result<FactorSourceIDFromHash> {
+        let gguid_raw = self.get_gguid_io(wallet).await?;
+
+        let bytes = Exactly32Bytes::try_from(gguid_raw)?;
+
+        Ok(FactorSourceIDFromHash::new(FactorSourceKind::ArculusCard, bytes))
     }
 
     async fn _create_wallet_seed(&self, wallet: ArculusWalletPointer, pin: String, word_count: u8) -> Result<Mnemonic> {
