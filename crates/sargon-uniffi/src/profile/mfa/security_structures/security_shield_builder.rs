e69d9bdb7b49e10b2eb5432392f058dfc89a7cf5
Alexander Cyon <alex.cyon@rdx.works>
2024-12-05 09:59:26 +0100

    Fix MethodTooLargeException by lowering method amount by 2.

--- crates/sargon-uniffi/src/profile/mfa/security_structures/security_shield_builder.rs
+++ crates/sargon-uniffi/src/profile/mfa/security_structures/security_shield_builder.rs
@@ -337,6 +337,26 @@ impl SecurityShieldBuilder {
         )
     }
 
+    // HERE BE DRAGONS
+    // Because of `MethodTooLargeException`, see my issue on UniFFIs Github:
+    // https://github.com/mozilla/uniffi-rs/issues/2340
+    //
+    // We need to temporarily restrict number of methods. So we merge three together into one.
+    // Once we have fixed the issue we can go back to use the three methods below.
+    pub fn validation_for_addition_of_factor_source_to_override_for_role_for_each_factor(
+        &self,
+        role_kind: RoleKind,
+        factor_sources: Vec<FactorSourceID>,
+    ) -> Vec<FactorSourceValidationStatus> {
+        match role_kind {
+            RoleKind::Primary => self.validation_for_addition_of_factor_source_to_primary_override_for_each(factor_sources),
+            RoleKind::Recovery => self.validation_for_addition_of_factor_source_to_recovery_override_for_each(factor_sources),
+            RoleKind::Confirmation => self.validation_for_addition_of_factor_source_to_confirmation_override_for_each(factor_sources),
+        }
+    }
+}
+
+impl SecurityShieldBuilder {
     pub fn validation_for_addition_of_factor_source_to_primary_override_for_each(
         &self,
         factor_sources: Vec<FactorSourceID>,
@@ -573,28 +593,35 @@ mod tests {
 
         assert_ne!(
             sim_prim,
-            sut.validation_for_addition_of_factor_source_to_primary_override_for_each(vec![
+            sut.validation_for_addition_of_factor_source_to_override_for_role_for_each_factor(
+                RoleKind::Primary,
+                vec![
                 FactorSourceID::sample_arculus(),
             ])
         );
 
         assert_ne!(
             sim_prim_threshold,
-            sut.validation_for_addition_of_factor_source_to_primary_threshold_for_each(vec![
+            sut.validation_for_addition_of_factor_source_to_primary_threshold_for_each(
+                vec![
                 FactorSourceID::sample_arculus()
             ])
         );
 
         assert_ne!(
             sim_rec,
-            sut.validation_for_addition_of_factor_source_to_recovery_override_for_each(vec![
-                FactorSourceID::sample_ledger(),
-            ])
-        );
+            sut.validation_for_addition_of_factor_source_to_override_for_role_for_each_factor(
+                RoleKind::Recovery,
+                vec![
+                    FactorSourceID::sample_ledger(),
+                    ])
+                );
 
         assert_ne!(
-            sim_conf,
-            sut.validation_for_addition_of_factor_source_to_confirmation_override_for_each(vec![
+                    sim_conf,
+                    sut.validation_for_addition_of_factor_source_to_override_for_role_for_each_factor(
+                RoleKind::Confirmation,
+                vec![
                 FactorSourceID::sample_device(),
             ])
         );
