3ee614f141059f48f324cdd4b3440a8f8a1ff194
Alexander Cyon <alex.cyon@rdx.works>
2024-12-05 12:28:52 +0100

    Simplify macro_rules impl which declared `MatrixOFFactor***` with `preinterpret` crate instead of `paste` crate.

--- crates/sargon-uniffi/src/profile/mfa/security_structures/matrices/decl_matrix_macro.rs
+++ crates/sargon-uniffi/src/profile/mfa/security_structures/matrices/decl_matrix_macro.rs
@@ -1,115 +1,89 @@
 use crate::prelude::*;
 
+use preinterpret::*;
+
 macro_rules! matrix_conversion {
-       // Impl From<Internal> -> crate
-       (from_internal: $internal:ident, $uniffi:ident) => {
-        impl From<$internal> for $uniffi {
-            fn from(value: $internal) -> Self {
-                    Self {
-            primary_role: value.primary().clone().into(),
-            recovery_role: value.recovery().clone().into(),
-            confirmation_role: value.confirmation().clone().into(),
-            number_of_days_until_auto_confirm: value
-                .number_of_days_until_auto_confirm,
-        }
-            }
-        }
-    };
-
-    // Impl From<crate> -> Internal
-    (to_internal: $internal_factor:ty => $uniffi:ident, $internal:ident) => {
-        impl $uniffi {
-            pub fn into_internal(&self) -> $internal {
-                unsafe {
-                    <$internal>::unbuilt_with_roles_and_days(
-                        self.primary_role.clone().into(),
-                        self.recovery_role.clone().into(),
-                        self.confirmation_role.clone().into(),
-                        self.number_of_days_until_auto_confirm,
-                    )
-                }
-            }
-        }
-        impl From<$uniffi> for $internal {
-            fn from(value: $uniffi) -> Self {
-               value.into_internal()
-            }
-        }
-    };
-       // Impl `From<InternalX> for X` and `From<X> for InternalX`
-    // and impl `HasSampleValues` for X` and UniFFI export `new_X_sample` and `new_X_sample_other`.
-    (impl_from: $factor_level:ty => $uniffi_name:ident => $internal_name:ident ) => {
-        matrix_conversion!(
-            from_internal: $internal_name, $uniffi_name
-        );
-        matrix_conversion!(
-            to_internal: $factor_level => $uniffi_name, $internal_name
-        );
-
-        impl HasSampleValues for $uniffi_name {
-            fn sample() -> Self {
-                $internal_name::sample().into()
-            }
-            fn sample_other() -> Self {
-                $internal_name::sample_other().into()
-            }
-        }
-
-        paste! {
-            #[uniffi::export]
-            pub fn [< new_ $uniffi_name:snake _ sample >]() -> $uniffi_name {
-                $uniffi_name::sample()
-            }
-
-            #[uniffi::export]
-            pub fn [< new_ $uniffi_name:snake _ sample_other >]() -> $uniffi_name {
-                $uniffi_name::sample_other()
-            }
-        }
-    };
     (
-        struct:
-        $(
-            #[doc = $expr: expr]
-        )*
-        $struct_name:ident, $factor_level:ty
+        // $(
+        //     #[doc = $expr: expr]
+        // )*
+        $(#[$attributes:meta])*
+        $factor_level: ident
     ) => {
-        paste! {
-            $(
-                #[doc = $expr]
-            )*
+        preinterpret::preinterpret! {
+            [!set! #internal_factor = [!ident! Internal $factor_level]]
+            [!set! #struct_name = [!ident! MatrixOf $factor_level s]]
+            [!set! #internal_struct_name = [!ident! Internal #struct_name]]
+            [!set! #primary_role_type = [!ident! PrimaryRoleWith $factor_level s ]]
+            [!set! #recovery_role_type = [!ident! RecoveryRoleWith $factor_level s ]]
+            [!set! #confirmation_role_type = [!ident! ConfirmationRoleWith $factor_level s ]]
+
+            use sargon::#struct_name as #internal_struct_name;
+            use sargon::$factor_level as #internal_factor;
+
+            $(#[$attributes])*
             #[derive(Clone, PartialEq, Eq, Hash, uniffi::Record)]
-            pub struct $struct_name {
-                pub primary_role: [< PrimaryRoleWith $factor_level s >],
-                pub recovery_role: [< RecoveryRoleWith $factor_level s >],
-                pub confirmation_role: [< ConfirmationRoleWith $factor_level s >],
+            pub struct #struct_name {
+                pub primary_role: #primary_role_type,
+                pub recovery_role: #recovery_role_type,
+                pub confirmation_role: #confirmation_role_type,
 
                 pub number_of_days_until_auto_confirm: u16,
             }
 
-            use sargon::$struct_name as [< Internal $struct_name>];
-            delegate_debug_into!($struct_name, [< Internal $struct_name>]);
-            matrix_conversion!(
-                impl_from: [< Internal $factor_level>] => $struct_name => [< Internal $struct_name >]
-            );
-        }
-    };
-    (
-        $(
-            #[doc = $expr: expr]
-        )*
-        $factor_level:ty
-    ) => {
-        paste! {
-            use sargon::$factor_level as [< Internal $factor_level>];
-            matrix_conversion!(
-                struct:
-                $(
-                    #[doc = $expr]
-                )*
-                [< MatrixOf $factor_level s >],
-                $factor_level
-            );
+            delegate_debug_into!(#struct_name, #internal_struct_name);
+
+            impl From<#internal_struct_name> for #struct_name {
+                fn from(value: #internal_struct_name) -> Self {
+                    Self {
+                        primary_role: value.primary().clone().into(),
+                        recovery_role: value.recovery().clone().into(),
+                        confirmation_role: value.confirmation().clone().into(),
+                        number_of_days_until_auto_confirm: value
+                            .number_of_days_until_auto_confirm,
+                    }
+                }
+            }
+
+            impl #struct_name {
+                pub fn into_internal(&self) -> #internal_struct_name {
+                    unsafe {
+                        #internal_struct_name::unbuilt_with_roles_and_days(
+                            self.primary_role.clone().into(),
+                            self.recovery_role.clone().into(),
+                            self.confirmation_role.clone().into(),
+                            self.number_of_days_until_auto_confirm,
+                        )
+                    }
+                }
+            }
+            impl From<#struct_name> for #internal_struct_name {
+                fn from(value: #struct_name) -> Self {
+                   value.into_internal()
+                }
+            }
+
+            impl HasSampleValues for #struct_name {
+                fn sample() -> Self {
+                    #internal_struct_name::sample().into()
+                }
+                fn sample_other() -> Self {
+                    #internal_struct_name::sample_other().into()
+                }
+            }
+
+            [!set! #fn_name_prefix = new_[!snake_case! #struct_name]]
+
+            #[uniffi::export]
+            pub fn [!ident! #fn_name_prefix _sample ]() -> #struct_name {
+                #struct_name::sample()
+            }
+
+            #[uniffi::export]
+            pub fn [!ident! #fn_name_prefix _sample_other ]() -> #struct_name {
+                #struct_name::sample_other()
+            }
+
         }
     };
 }
