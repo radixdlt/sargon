1eacf57ed8ef36a59c6b613ba36b935260920bfe
micbakos-rdx <michael.bakogiannis@rdx.works>
2024-12-09 18:28:04 +0200

    Expose RolaChallenge in kotlin

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/RolaChallenge.kt
+++ jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/RolaChallenge.kt
@@ -1,2 +1,7 @@
 package com.radixdlt.sargon.extensions
 
+import com.radixdlt.sargon.Hash
+import com.radixdlt.sargon.RolaChallenge
+import com.radixdlt.sargon.rolaChallengeGetHash
+
+fun RolaChallenge.hash(): Hash = rolaChallengeGetHash(rolaChallenge = this)
