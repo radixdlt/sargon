b28d6ebcc6636e8420b3e7d61450b208f815ed21
Alexander Cyon <alex.cyon@gmail.com>
2024-12-29 21:02:55 +0100

    try fix kotlin

--- jvm/sargon-android/src/test/java/com/radixdlt/sargon/FactorSourceIdTest.kt
+++ jvm/sargon-android/src/test/java/com/radixdlt/sargon/FactorSourceIdTest.kt
@@ -4,6 +4,7 @@ import com.radixdlt.sargon.extensions.asGeneral
 import com.radixdlt.sargon.extensions.fromJson
 import com.radixdlt.sargon.extensions.hex
 import com.radixdlt.sargon.extensions.init
+import com.radixdlt.sargon.extensions.string
 import com.radixdlt.sargon.extensions.randomBagOfBytes
 import com.radixdlt.sargon.extensions.toJson
 import com.radixdlt.sargon.samples.sampleMainnet
--- jvm/sargon-android/src/test/java/com/radixdlt/sargon/FactorSourceTest.kt
+++ jvm/sargon-android/src/test/java/com/radixdlt/sargon/FactorSourceTest.kt
@@ -10,6 +10,7 @@ import com.radixdlt.sargon.extensions.olympia
 import com.radixdlt.sargon.extensions.supportsBabylon
 import com.radixdlt.sargon.extensions.supportsOlympia
 import com.radixdlt.sargon.extensions.name
+import com.radixdlt.sargon.extensions.string
 import com.radixdlt.sargon.samples.Sample
 import com.radixdlt.sargon.samples.sample
 import com.radixdlt.sargon.samples.sampleMainnet
