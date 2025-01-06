b3da9bf049e75d9631164dff2740805950c2ca90
giannis.tsepas <giannis.tsepas@rdx.works>
2025-01-06 18:29:20 +0200

    removed

--- apple/Sources/Sargon/Extensions/Methods/Profile/Factor/FactorSourceID+Wrap+Functions.swift
+++ apple/Sources/Sargon/Extensions/Methods/Profile/Factor/FactorSourceID+Wrap+Functions.swift
@@ -5,12 +5,4 @@ extension FactorSourceID {
 	public func toString() -> String {
 		factorSourceIdToString(factorSourceId: self)
 	}
-
-	public init(jsonData: some DataProtocol) throws {
-		self = try newFactorSourceIDFromJsonBytes(jsonBytes: Data(jsonData))
-	}
-
-	public func jsonData() -> Data {
-		factorSourceIDsToJsonBytes(factorSourceID: self)
-	}
 }
