f066cd48b3d0e326b2829de261bc836c98eaf0af
Alexander Cyon <alex.cyon@gmail.com>
2025-01-07 09:48:37 +0100

    reintroduce tests

--- crates/app/signing-traits/src/authentication/signed_auth_intent.rs
+++ crates/app/signing-traits/src/authentication/signed_auth_intent.rs
@@ -63,68 +63,68 @@ impl HasSampleValues for SignedAuthIntent {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[allow(clippy::upper_case_acronyms)]
-//     type SUT = SignedAuthIntent;
+    #[allow(clippy::upper_case_acronyms)]
+    type SUT = SignedAuthIntent;
 
-//     #[test]
-//     fn equality() {
-//         assert_eq!(SUT::sample(), SUT::sample());
-//         assert_eq!(SUT::sample_other(), SUT::sample_other());
-//     }
+    #[test]
+    fn equality() {
+        assert_eq!(SUT::sample(), SUT::sample());
+        assert_eq!(SUT::sample_other(), SUT::sample_other());
+    }
 
-//     #[test]
-//     fn inequality() {
-//         assert_ne!(SUT::sample(), SUT::sample_other());
-//     }
+    #[test]
+    fn inequality() {
+        assert_ne!(SUT::sample(), SUT::sample_other());
+    }
 
-//     #[test]
-//     fn test_valid_signatures() {
-//         let intent = AuthIntent::sample();
-//         let mnemonic_with_passphrase = MnemonicWithPassphrase::sample();
+    #[test]
+    fn test_valid_signatures() {
+        let intent = AuthIntent::sample();
+        let mnemonic_with_passphrase = MnemonicWithPassphrase::sample();
 
-//         let signature = mnemonic_with_passphrase
-//             .sign(&intent.auth_intent_hash().hash(), &DerivationPath::sample());
-//         let intent_signatures = IndexMap::kv(
-//             AddressOfAccountOrPersona::sample(),
-//             IntentSignature(signature),
-//         );
-//         assert!(SignedAuthIntent::new(intent, intent_signatures).is_ok())
-//     }
+        let signature = mnemonic_with_passphrase
+            .sign(&intent.auth_intent_hash().hash(), &DerivationPath::sample());
+        let intent_signatures = IndexMap::kv(
+            AddressOfAccountOrPersona::sample(),
+            IntentSignature(signature),
+        );
+        assert!(SignedAuthIntent::new(intent, intent_signatures).is_ok())
+    }
 
-//     #[test]
-//     fn test_invalid_signatures() {
-//         let intent_signatures = IndexMap::kv(
-//             AddressOfAccountOrPersona::sample(),
-//             IntentSignature::sample(),
-//         );
-//         assert_eq!(
-//             SUT::new(AuthIntent::sample(), intent_signatures),
-//             Err(CommonError::InvalidSignaturesForIntentSomeDidNotValidateIntentHash)
-//         )
-//     }
+    #[test]
+    fn test_invalid_signatures() {
+        let intent_signatures = IndexMap::kv(
+            AddressOfAccountOrPersona::sample(),
+            IntentSignature::sample(),
+        );
+        assert_eq!(
+            SUT::new(AuthIntent::sample(), intent_signatures),
+            Err(CommonError::InvalidSignaturesForIntentSomeDidNotValidateIntentHash)
+        )
+    }
 
-//     #[test]
-//     fn test_get_intent() {
-//         let intent = AuthIntent::sample();
-//         let mnemonic_with_passphrase = MnemonicWithPassphrase::sample();
+    #[test]
+    fn test_get_intent() {
+        let intent = AuthIntent::sample();
+        let mnemonic_with_passphrase = MnemonicWithPassphrase::sample();
 
-//         let signature = mnemonic_with_passphrase
-//             .sign(&intent.auth_intent_hash().hash(), &DerivationPath::sample());
-//         let intent_signatures = IndexMap::kv(
-//             AddressOfAccountOrPersona::sample(),
-//             IntentSignature(signature),
-//         );
+        let signature = mnemonic_with_passphrase
+            .sign(&intent.auth_intent_hash().hash(), &DerivationPath::sample());
+        let intent_signatures = IndexMap::kv(
+            AddressOfAccountOrPersona::sample(),
+            IntentSignature(signature),
+        );
 
-//         assert_eq!(
-//             SUT::new(intent.clone(), intent_signatures)
-//                 .unwrap()
-//                 .intent()
-//                 .clone(),
-//             intent
-//         )
-//     }
-// }
+        assert_eq!(
+            SUT::new(intent.clone(), intent_signatures)
+                .unwrap()
+                .intent()
+                .clone(),
+            intent
+        )
+    }
+}
