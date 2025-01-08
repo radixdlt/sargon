bb075efff05450ddca8938e130189e7352c98b7e
Alexander Cyon <alex.cyon@gmail.com>
2025-01-08 10:06:22 +0100

    skip push

--- scripts/bump_and_commit.sh
+++ scripts/bump_and_commit.sh
@@ -3,4 +3,4 @@
 set -e
 set -u
 
-cargo ws version patch --allow-branch $(git symbolic-ref --short HEAD) --yes --no-git-tag
+cargo ws version patch --allow-branch $(git symbolic-ref --short HEAD) --yes --no-git-tag --no-git-push
