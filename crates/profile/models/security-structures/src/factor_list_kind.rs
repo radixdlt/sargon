1f03e215f18c0447e37c6156ca74d0f2988194dc
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2025-01-08 23:18:43 +0200

    Add sampls and tests

--- crates/profile/models/security-structures/src/factor_list_kind.rs
+++ crates/profile/models/security-structures/src/factor_list_kind.rs
@@ -1,6 +1,37 @@
+use has_sample_values::HasSampleValues;
+
 /// A kind of factor list, either threshold, or override kind.
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum FactorListKind {
     Threshold,
     Override,
+}
+
+impl HasSampleValues for FactorListKind {
+    fn sample() -> Self {
+        FactorListKind::Threshold
+    }
+
+    fn sample_other() -> Self {
+        FactorListKind::Override
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(clippy::upper_case_acronyms)]
+    type SUT = FactorListKind;
+
+    #[test]
+    fn equality() {
+        assert_eq!(SUT::sample(), SUT::sample());
+        assert_eq!(SUT::sample_other(), SUT::sample_other());
+    }
+
+    #[test]
+    fn inequality() {
+        assert_ne!(SUT::sample(), SUT::sample_other());
+    }
 }
