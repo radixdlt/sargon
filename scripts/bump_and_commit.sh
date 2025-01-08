72df12f7880b2399f16501051b39c659397eab84
Alexander Cyon <alex.cyon@gmail.com>
2025-01-08 09:58:49 +0100

    add script to bump

---
+++ scripts/bump_and_commit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+set -e
+set -u
+
+cargo ws version patch --allow-branch $(git symbolic-ref --short HEAD) --yes --no-git-tag
---
+++ scripts/bump_no_commit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+set -e
+set -u
+
+cargo ws version patch --no-git-commit --yes
