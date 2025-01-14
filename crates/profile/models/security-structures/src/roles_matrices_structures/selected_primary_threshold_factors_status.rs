49bab001f5b7cabd4ecc787a35e5eb4c57d680e8
Sergiu Puhalschi <sergiu.puhalschi@rdx.works>
2025-01-14 14:45:12 +0200

    Add tests

--- crates/profile/models/security-structures/src/roles_matrices_structures/selected_primary_threshold_factors_status.rs
+++ crates/profile/models/security-structures/src/roles_matrices_structures/selected_primary_threshold_factors_status.rs
@@ -53,3 +53,22 @@ impl HasSampleValues for SelectedPrimaryThresholdFactorsStatusInvalidReason {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[allow(clippy::upper_case_acronyms)]
+    type SUT = SelectedPrimaryThresholdFactorsStatusInvalidReason;
+
+    #[test]
+    fn equality() {
+        assert_eq!(SUT::sample(), SUT::sample());
+        assert_eq!(SUT::sample_other(), SUT::sample_other());
+    }
+
+    #[test]
+    fn inequality() {
+        assert_ne!(SUT::sample(), SUT::sample_other());
+    }
+}
