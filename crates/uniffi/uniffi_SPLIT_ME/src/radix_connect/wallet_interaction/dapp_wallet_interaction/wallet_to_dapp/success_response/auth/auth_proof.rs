85d77dd17fd6df0d8c98b0066c575d5e8daddfb5
Matias Bzurovski <matias.bzurovski@rdx.works>
2025-01-28 14:41:31 +0100

    create WalletToDappInteractionAuthProof on Sargon

--- crates/uniffi/uniffi_SPLIT_ME/src/radix_connect/wallet_interaction/dapp_wallet_interaction/wallet_to_dapp/success_response/auth/auth_proof.rs
+++ crates/uniffi/uniffi_SPLIT_ME/src/radix_connect/wallet_interaction/dapp_wallet_interaction/wallet_to_dapp/success_response/auth/auth_proof.rs
@@ -7,3 +7,16 @@ pub struct WalletToDappInteractionAuthProof {
     pub curve: SLIP10Curve,
     pub signature: Signature,
 }
+
+#[uniffi::export]
+pub fn new_wallet_to_dapp_interaction_auth_proof_from_intent_signature_of_owner(
+    intent_signature_of_owner: IntentSignatureOfOwner,
+) -> WalletToDappInteractionAuthProof {
+    InternalWalletToDappInteractionAuthProof::from(
+        intent_signature_of_owner
+            .intent_signature
+            .value
+            .into_internal(),
+    )
+    .into()
+}
