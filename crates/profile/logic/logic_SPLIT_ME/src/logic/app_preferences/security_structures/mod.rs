34553b40582790bb3f73aa63ce5cb7f8e462f68e
giannis.tsepas <giannis.tsepas@rdx.works>
2025-02-07 15:36:00 +0200

    moved functions and models not related to the profile to profile-logic

---
+++ crates/profile/logic/logic_SPLIT_ME/src/logic/app_preferences/security_structures/mod.rs
@@ -0,0 +1,7 @@
+mod entities_linked_to_security_structure;
+mod security_structures_update;
+mod shield_for_display;
+
+pub use entities_linked_to_security_structure::*;
+pub use security_structures_update::*;
+pub use shield_for_display::*;
