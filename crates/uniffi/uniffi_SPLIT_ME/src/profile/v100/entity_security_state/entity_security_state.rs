c50c2d5d7117a1019072e40b0173a24f366e9aec
micbakos-rdx <michael.bakogiannis@rdx.works>
2025-02-07 15:24:28 +0200

    Use InternalConversion for EntitySecurityState

--- crates/uniffi/uniffi_SPLIT_ME/src/profile/v100/entity_security_state/entity_security_state.rs
+++ crates/uniffi/uniffi_SPLIT_ME/src/profile/v100/entity_security_state/entity_security_state.rs
@@ -4,7 +4,7 @@ use sargon::EntitySecurityState as InternalEntitySecurityState;
 /// Describes the state an entity - Account or Persona - is in, in regards to how
 /// the user controls it, i.e. if it is controlled by a single factor (private key)
 ///  or an `AccessController` with a potential Multi-Factor setup.
-#[derive(Clone, PartialEq, Eq, Hash, uniffi::Enum)]
+#[derive(Clone, PartialEq, Eq, Hash, InternalConversion, uniffi::Enum)]
 #[allow(clippy::large_enum_variant)]
 pub enum EntitySecurityState {
     /// The entity is controlled by a single factor (private key)
@@ -12,45 +12,6 @@ pub enum EntitySecurityState {
 
     /// The entity is controlled by multi-factor
     Securified { value: SecuredEntityControl },
-}
-
-impl EntitySecurityState {
-    pub fn into_internal(&self) -> InternalEntitySecurityState {
-        self.clone().into()
-    }
-}
-impl From<InternalEntitySecurityState> for EntitySecurityState {
-    fn from(value: InternalEntitySecurityState) -> Self {
-        match value {
-            InternalEntitySecurityState::Unsecured { value } => {
-                EntitySecurityState::Unsecured {
-                    value: value.into(),
-                }
-            }
-            InternalEntitySecurityState::Securified { value } => {
-                EntitySecurityState::Securified {
-                    value: value.into(),
-                }
-            }
-        }
-    }
-}
-
-impl From<EntitySecurityState> for InternalEntitySecurityState {
-    fn from(val: EntitySecurityState) -> Self {
-        match val {
-            EntitySecurityState::Unsecured { value } => {
-                InternalEntitySecurityState::Unsecured {
-                    value: value.into_internal(),
-                }
-            }
-            EntitySecurityState::Securified { value } => {
-                InternalEntitySecurityState::Securified {
-                    value: value.into_internal(),
-                }
-            }
-        }
-    }
 }
 
 #[uniffi::export]
