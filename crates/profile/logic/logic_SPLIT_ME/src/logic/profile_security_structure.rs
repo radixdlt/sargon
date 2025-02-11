7dee6237ea74fd8b367d6c279c6582f76412e290
danvleju-rdx <163979791+danvleju-rdx@users.noreply.github.com>
2025-02-11 15:06:25 +0200

    feedback updates

--- crates/profile/logic/logic_SPLIT_ME/src/logic/profile_security_structure.rs
+++ crates/profile/logic/logic_SPLIT_ME/src/logic/profile_security_structure.rs
@@ -5,13 +5,25 @@ pub trait ProfileHasAnyMainSecurityStructure {
     fn has_any_main_security_structure(&self) -> bool;
 }
 
+impl ProfileHasAnyMainSecurityStructure for Security {
+    /// Returns true if the profile has any main security structure.
+    fn has_any_main_security_structure(&self) -> bool {
+        self.security_structures_of_factor_source_ids
+            .iter()
+            .any(|s| s.is_main())
+    }
+}
+
+impl ProfileHasAnyMainSecurityStructure for AppPreferences {
+    /// Returns true if the profile has any main security structure.
+    fn has_any_main_security_structure(&self) -> bool {
+        self.security.has_any_main_security_structure()
+    }
+}
+
 impl ProfileHasAnyMainSecurityStructure for Profile {
     /// Returns true if the profile has any main security structure.
     fn has_any_main_security_structure(&self) -> bool {
-        self.app_preferences
-            .security
-            .security_structures_of_factor_source_ids
-            .iter()
-            .any(|s| s.is_main())
+        self.app_preferences.has_any_main_security_structure()
     }
 }
