a44c0203ab4d7dd2bea0eeffc009445e5b06f54a
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2025-02-13 12:06:23 +0200

    wip

--- crates/uniffi/uniffi_SPLIT_ME/src/wrapped_radix_engine_toolkit/low_level/execution_summary/execution_summary.rs
+++ crates/uniffi/uniffi_SPLIT_ME/src/wrapped_radix_engine_toolkit/low_level/execution_summary/execution_summary.rs
@@ -26,8 +26,7 @@ pub struct ExecutionSummary {
     /// Information on the global entities created in the transaction.
     pub new_entities: NewEntities,
 
-    /// The various classifications that this manifest matched against. Note
-    /// that an empty set means that the manifest is non-conforming.
+    /// The manifest classification if any.
     pub detailed_classification: Option<DetailedManifestClass>,
 
     /// List of newly created Non-Fungibles during this transaction.
