33603cf8c83516b1d65609c55fa2290db947c085
Matias Bzurovski <matias.bzurovski@rdx.works>
2025-02-24 18:39:45 +0100

    more tests

--- crates/app/interaction-queue/src/lib.rs
+++ crates/app/interaction-queue/src/lib.rs
@@ -9,10 +9,10 @@ mod test_helpers;
 pub mod prelude {
     pub use crate::batch::*;
     pub use crate::item::*;
+    pub use crate::manager::*;
     pub use crate::observer::*;
     pub use crate::queue::*;
     pub use crate::storage::*;
-    pub use crate::manager::*;
 
     pub(crate) use core_utils::prelude::*;
     pub(crate) use gateway_client_and_api::prelude::*;
