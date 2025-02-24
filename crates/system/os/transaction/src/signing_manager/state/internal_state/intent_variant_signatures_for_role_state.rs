65c32c863334d216b6d1820cc3f583120a4c2a54
Alexander Cyon <alex.cyon@rdx.works>
2025-02-24 09:43:51 +0100

    [no ci] wip

--- crates/system/os/transaction/src/signing_manager/state/internal_state/intent_variant_signatures_for_role_state.rs
+++ crates/system/os/transaction/src/signing_manager/state/internal_state/intent_variant_signatures_for_role_state.rs
@@ -3,7 +3,16 @@ use crate::prelude::*;
 #[derive(Clone, PartialEq, Eq, derive_more::Debug)]
 #[debug("#{} entities w sigs", self.signatures_per_entity.len())]
 pub(crate) struct IntentVariantSignaturesForRoleState {
-    role: RoleKind,
+    /// Not present when initialized, but set when the first entity is added.
+    /// and then used to assert that all entities have the same intent.
+    intent: Option<TransactionIntentHash>,
+    role: Immutable<RoleKind>,
+
+    /// A bit confusing perhaps since you dont skip a entity, you skip a factor,
+    /// perhaps this should be transposed to be a map of factors to entities...
+    neglected_factors_per_entity:
+        IndexMap<AddressOfAccountOrPersona, IndexSet<NeglectedFactor>>,
+
     signatures_per_entity:
         IndexMap<AddressOfAccountOrPersona, IndexSet<SignatureWithPublicKey>>,
 }
@@ -26,19 +35,32 @@ impl IntentVariantSignaturesForRoleState {
         Ok(self.signatures_per_entity.clone())
     }
 
+    fn set_intent_else_assert(&mut self, intent: &TransactionIntent) {
+        if let Some(existing_intent) = self.intent.as_ref() {
+            assert_eq!(*existing_intent, intent.transaction_intent_hash());
+        } else {
+            self.intent = Some(intent.transaction_intent_hash());
+        }
+    }
+
     pub(crate) fn update_with_entity_not_signed_for(
         &mut self,
         not_signed: EntityNotSignedFor,
     ) {
-        assert_eq!(not_signed.role_kind(), self.role);
-        todo!("Implement");
+        assert_eq!(not_signed.role_kind(), *self.role);
+        self.set_intent_else_assert(&not_signed.intent);
+        self.neglected_factors_per_entity.append_or_insert_to(
+            not_signed.entity.address(),
+            not_signed.neglected_factor_sources(),
+        );
     }
 
     pub(crate) fn update_with_intent_with_signatures(
         &mut self,
         intent_with_signatures: EntitySignedFor,
     ) {
-        assert_eq!(intent_with_signatures.role_kind(), self.role);
+        assert_eq!(intent_with_signatures.role_kind(), *self.role);
+        self.set_intent_else_assert(&intent_with_signatures.intent);
         self.signatures_per_entity.append_or_insert_to(
             intent_with_signatures.entity.address(),
             intent_with_signatures
@@ -49,10 +71,12 @@ impl IntentVariantSignaturesForRoleState {
         );
     }
 
-    pub(crate) fn new(role: RoleKind) -> Self {
+    pub(crate) fn new(role: impl Into<Immutable<RoleKind>>) -> Self {
         Self {
-            role,
+            intent: None,
+            role: role.into(),
             signatures_per_entity: IndexMap::new(),
+            neglected_factors_per_entity: IndexMap::new(),
         }
     }
 }
