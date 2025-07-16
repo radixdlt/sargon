89da86284e08f1eafefe007b810a18caf67090ac
Ghenadie <118184705+GhenadieVP@users.noreply.github.com>
2025-07-16 09:23:06 +0300

    Rewards home card (#411)
    * fix blog posts fetching
    
    * Add JoinRadixRewards home card
    
    * fix tests
    
    * default to empty
    
    * clippy
    
    * clippy
    
    * set nextest version
    
    * correct version
    
    * bump toolchain
    
    * Revert "bump toolchain"
    
    This reverts commit b443f424315f5106cc976e79cb1b38fb232a4000.
    
    * version
    
    * swiftformat
    
    * kt home cards storage impl update
    
    * Release 1.2.22
    
    account-for-display@1.2.22
    addresses@1.2.22
    assert-json@1.2.22
    build-info@1.2.22
    bytes@1.2.22
    cap26-models@1.2.22
    clients@1.2.22
    core-collections@1.2.22
    core-misc@1.2.22
    core-utils@1.2.22
    discover@1.2.22
    drivers@1.2.22
    ecc@1.2.22
    encryption@1.2.22
    entity-by-address@1.2.22
    entity-foundation@1.2.22
    error@1.2.22
    factor-instances-provider@1.2.22
    factors@1.2.22
    factors-supporting-types@1.2.22
    gateway-client-and-api@1.2.22
    gateway-models@1.2.22
    has-sample-values@1.2.22
    hash@1.2.22
    hierarchical-deterministic@1.2.22
    home-cards@1.2.22
    host-info@1.2.22
    http-client@1.2.22
    identified-vec-of@1.2.22
    interactors@1.2.22
    key-derivation-traits@1.2.22
    keys-collector@1.2.22
    manifests@1.2.22
    metadata@1.2.22
    network@1.2.22
    next-derivation-index-ephemeral@1.2.22
    numeric@1.2.22
    prelude@1.2.22
    profile@1.2.22
    profile-account@1.2.22
    profile-account-or-persona@1.2.22
    profile-app-preferences@1.2.22
    profile-base-entity@1.2.22
    profile-gateway@1.2.22
    profile-logic@1.2.22
    profile-persona@1.2.22
    profile-persona-data@1.2.22
    profile-security-structures@1.2.22
    profile-state-holder@1.2.22
    profile-supporting-types@1.2.22
    radix-connect@1.2.22
    radix-connect-models@1.2.22
    radix-name-service@1.2.22
    sargon@1.2.22
    sargon-os@1.2.22
    sargon-os-accounts@1.2.22
    sargon-os-derive-public-keys@1.2.22
    sargon-os-factors@1.2.22
    sargon-os-security-center@1.2.22
    sargon-os-signing@1.2.22
    sargon-os-transaction@1.2.22
    sargon-uniffi@1.2.22
    sargon-uniffi-conversion-macros@1.2.22
    security-center@1.2.22
    short-string@1.2.22
    signatures-collector@1.2.22
    signing-traits@1.2.22
    sub-systems@1.2.22
    time-utils@1.2.22
    transaction-foundation@1.2.22
    transaction-models@1.2.22
    
    Generated by cargo-workspaces

--- crates/app/home-cards/src/storage.rs
+++ crates/app/home-cards/src/storage.rs
@@ -9,4 +9,13 @@ pub trait HomeCardsStorage: Send + Sync {
 
     /// Loads the encoded home cards from the storage.
     async fn load_cards(&self) -> Result<Option<BagOfBytes>>;
+
+    /// Saves the encoded dismissed home cards to the storage.
+    async fn save_dismissed_cards(
+        &self,
+        encoded_cards: BagOfBytes,
+    ) -> Result<()>;
+
+    /// Loads the encoded dismissed home cards from the storage.
+    async fn load_dismissed_cards(&self) -> Result<Option<BagOfBytes>>;
 }
