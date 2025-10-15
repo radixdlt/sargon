102cec0eede1a3c7f4606b46d0d01a8af78744a2
Sergiu Puhalschi <sergiu@radixpublishing.com>
2025-10-15 14:06:54 +0300

    Confirm timed recovery

--- crates/transaction/manifests/src/lib.rs
+++ crates/transaction/manifests/src/lib.rs
@@ -12,7 +12,6 @@ mod summary;
 mod third_party_deposit_update;
 
 pub mod prelude {
-
     pub(crate) use crate::bucket_factory;
 
     pub use crate::delete_account::*;
@@ -39,7 +38,7 @@ pub mod prelude {
     pub(crate) use radix_common::{
         math::Decimal as ScryptoDecimal192,
         prelude::{
-            ScryptoValue, ACCOUNT_OWNER_BADGE as SCRYPTO_ACCOUNT_OWNER_BADGE,
+            ACCOUNT_OWNER_BADGE as SCRYPTO_ACCOUNT_OWNER_BADGE,
             IDENTITY_OWNER_BADGE as SCRYPTO_IDENTITY_OWNER_BADGE,
         },
         ManifestSbor as ScryptoManifestSbor, ScryptoSbor,
