8b8609c41729d6b0ea64465249db86ef98fe4cc6
Sergiu Puhalschi <sergiu@radixpublishing.com>
2025-10-21 15:13:12 +0300

    Uniffi expose confirm & stop

--- crates/uniffi/uniffi_SPLIT_ME/src/system/sargon_os/sargon_os_stop_timed_recovery_interaction.rs
+++ crates/uniffi/uniffi_SPLIT_ME/src/system/sargon_os/sargon_os_stop_timed_recovery_interaction.rs
@@ -2,12 +2,12 @@ use crate::prelude::*;
 
 #[uniffi::export]
 impl SargonOS {
-    pub async fn make_confirm_timed_recovery_manifest(
+    pub async fn make_stop_timed_recovery_manifest(
         &self,
         address: AddressOfAccountOrPersona,
     ) -> Result<TransactionManifest> {
         self.wrapped
-            .make_confirm_timed_recovery_manifest(address.into())
+            .make_stop_timed_recovery_manifest(address.into())
             .await
             .into_result()
     }
