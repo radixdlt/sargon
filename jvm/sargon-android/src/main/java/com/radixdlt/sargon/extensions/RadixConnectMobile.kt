758530087d8c39c4c4cffd9b646d2d05ca5e9b64
Sergiu Puhalschi <sergiu@radixpublishing.com>
2026-02-25 15:59:08 +0200

    Delete unnecessary kotlin extension function

--- jvm/sargon-android/src/main/java/com/radixdlt/sargon/extensions/RadixConnectMobile.kt
+++
@@ -1,7 +0,0 @@
-package com.radixdlt.sargon.extensions
-
-import com.radixdlt.sargon.SargonOs
-import com.radixdlt.sargon.annotation.KoverIgnore
-
-@KoverIgnore
-fun SargonOs.radixConnectMobile() = radixConnectMobile()
