996f1452dbd0b04563d7c48521051d5a82c3eaf5
GhenadieVP <ghenadie@radixpublishing.com>
2026-02-28 13:54:56 +0200

    rcr crud api

--- crates/uniffi/uniffi_SPLIT_ME/src/system/sargon_os/sargon_os_relay_service.rs
+++ crates/uniffi/uniffi_SPLIT_ME/src/system/sargon_os/sargon_os_relay_service.rs
@@ -13,6 +13,36 @@ impl SargonOS {
 
 #[uniffi::export]
 impl SargonOS {
+    pub async fn add_relay_service(
+        &self,
+        service: RelayService,
+    ) -> Result<bool> {
+        self.wrapped
+            .add_relay_service(service.into())
+            .await
+            .into_result()
+    }
+
+    pub async fn update_relay_service(
+        &self,
+        updated: RelayService,
+    ) -> Result<bool> {
+        self.wrapped
+            .update_relay_service(updated.into())
+            .await
+            .into_result()
+    }
+
+    pub async fn delete_relay_service(
+        &self,
+        service: RelayService,
+    ) -> Result<bool> {
+        self.wrapped
+            .delete_relay_service(service.into())
+            .await
+            .into_result()
+    }
+
     pub async fn change_current_relay_service(
         &self,
         to: RelayService,
